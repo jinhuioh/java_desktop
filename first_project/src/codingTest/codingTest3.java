00aa89be4bfc58323cf9d511f7ae39d48ace2c46
OJH <aaa779979@naver.com>
2022-12-27 00:33:11 +0900

    codingTest

--- first_project/src/codingTest/codingTest3.java
+++ first_project/src/codingTest/codingTest3.java
@@ -50,7 +50,49 @@ public class codingTest3 {
 			} // for
 		} // for
 		
-
+		// - 개수
+		// -나오면 count = 1로 바꿨다가 ㅣ 나오면 count = 0으로 초기화
+		int count = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+//				System.out.println("arr[i][j]>>"+i+j+arr[i][j]);
+				if(arr[i][j] == '|') {
+//					System.out.println("l인경우 count>> "+count);
+//					System.out.println("l인경우 answer>> "+answer);
+					count=0;
+				}//if
+				//행이 바뀌는데 첫번째-의 경우 count의 값을 0으로 초기화
+				else if(j == 0 && arr[i][j] == '-') {
+					count=0;
+				}//else if
+				if(count == 0 && arr[i][j] == '-') {
+					count=1;
+					//정답 개수 증가 시키기
+					answer++;
+//					System.out.println("-인경우 count>> "+count);
+//					System.out.println("-인경우 answer>> "+answer);
+				}//else if
+			}//for
+		}//for
 		
+		// | 개수
+		int count2 =0;
+		for (int j = 0; j < m; j++) {
+			for (int i = 0; i < n; i++) {
+				if(arr[i][j] == '-') {
+					count2=0;
+				}//if
+				//열이 바뀌고 행이1인데 이전 열 마지막값과 다음열 첫번째 값이 |인 경우
+				else if(i == 0 && arr[i][j] == '|') {
+					count2=0;
+				}//else if
+				if(count2 == 0 && arr[i][j] == '|') {
+					count2=1;
+					//정답 개수 증가 시키기
+					answer++;
+				}//else if
+			}//for
+		}//for
+		System.out.println(answer);
 	    }
 }
