7fef032d8b4a1c5cff1d4d224102b0e8b2ff660e
OJH <aaa779979@naver.com>
2022-12-29 01:35:04 +0900

    codingTest

--- first_project/src/codingTest/codingTest3.java
+++ first_project/src/codingTest/codingTest3.java
@@ -8,129 +8,55 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
-//배추
+//트리의 부모 찾기
+//문제
+//루트 없는 트리가 주어진다. 이때, 트리의 루트를 1이라고 정했을 때, 각 노드의 부모를 구하는 프로그램을 작성하시오.
+//
 //입력
-//입력의 첫 줄에는 테스트 케이스의 개수 T가 주어진다. 그 다음 줄부터 각각의 테스트 케이스에 대해 첫째 줄에는 배추를 심은 배추밭의 가로길이 M(1 ≤ M ≤ 50)과 세로길이 N(1 ≤ N ≤ 50),
-//그리고 배추가 심어져 있는 위치의 개수 K(1 ≤ K ≤ 2500)이 주어진다. 그 다음 K줄에는 배추의 위치 X(0 ≤ X ≤ M-1), Y(0 ≤ Y ≤ N-1)가 주어진다. 두 배추의 위치가 같은 경우는 없다.
+//첫째 줄에 노드의 개수 N (2 ≤ N ≤ 100,000)이 주어진다. 둘째 줄부터 N-1개의 줄에 트리 상에서 연결된 두 정점이 주어진다.
 //
 //출력
-//각 테스트 케이스에 대해 필요한 최소의 배추흰지렁이 마리 수를 출력한다.
-//
-//예제 입력 1 
-//2
-//10 8 17
-//0 0
-//1 0
-//1 1
-//4 2
-//4 3
-//4 5
-//2 4
-//3 4
-//7 4
-//8 4
-//9 4
-//7 5
-//8 5
-//9 5
-//7 6
-//8 6
-//9 6
-//10 10 1
-//5 5
-//예제 출력 1 
-//5
-//1
+//첫째 줄부터 N-1개의 줄에 각 노드의 부모 노드 번호를 2번 노드부터 순서대로 출력한다.
 public class codingTest3 {
-	static int c;//테스트 개수
-	static int n;//세로
-	static int m;//가로
-	static int k;//배추 개수
-	static int count;
-	static int[][] map;//배추 표시할 배열
-	static boolean[][] visited;//탐색했는지 판단할 배열
-	static int[] dx = {0,1,0,-1};
-	static int[] dy = {1,0,-1,0};
-	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		
-		Queue<Integer> one = new LinkedList<Integer>();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		c = Integer.parseInt(br.readLine());
-		for(int i1=0; i1<c; i1++) {
-			int count = 0;
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			m = Integer.parseInt(st.nextToken());
-			n = Integer.parseInt(st.nextToken());
-			k = Integer.parseInt(st.nextToken());
-			map = new int[m][n];
-			visited = new boolean[m][n];
-			for(int i=0; i<k; i++) {
-				StringTokenizer st1 = new StringTokenizer(br.readLine());
-				int y = Integer.parseInt(st1.nextToken());//가로 위치
-				int x = Integer.parseInt(st1.nextToken());//세로 위치
-				map[y][x]=1;//배추 표시
-			}//for
-			bfs();
-			
+		int n = Integer.parseInt(br.readLine());
+		
+		ArrayList<ArrayList<Integer>> map = new ArrayList<>();
+		for(int i=0; i<=n; i++) {
+			map.add(new ArrayList<>());
+		}
+		for(int i1=1; i1<n; i1++) { 	
+			StringTokenizer st = new StringTokenizer(br.readLine()," ");
+			int p = Integer.parseInt(st.nextToken());//부모
+			int k = Integer.parseInt(st.nextToken());//자식
+			map.get(p).add(k);
+			map.get(k).add(p);
+			System.out.println(map);
 		}//for
-
-		sb.setLength(sb.length()-1);
-		System.out.println(sb);
+		
+		boolean[] visited = new boolean[n+1];//방문했는지 채크
+		int[] p = new int[n+1];//부모 노드 저장//정답이 되는 배열
+	
+		//bfs
+//		1번부터 시작
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.add(1);
+		while(!q.isEmpty()) {
+			int v = q.poll();
+			for(int nums: map.get(v)) {// v번째 인덱스의 자식을 nums변수에 하나씩 넣어줌
+				if(!visited[nums]) {//자식을 방문한 적 없으면
+					visited[nums]=true;
+					//부모 지정
+					p[nums] = v;
+					//다음 자식으로 이동
+					q.add(nums);
+				}
+			}
+		}//while
+		for(int i2=2; i2<=n; i2++) {
+			System.out.println(p[i2]);
+		}
 		
 	}//public
-	
-	private static void bfs() {
-	int count = 0;
-	//0,0부터 탐색
-	for(int i=0; i<m; i++) {
-		for(int j=0; j<n; j++) {
-//			System.out.println(i+" "+j);
-//			System.out.println("배추가 있는가?>>"+map[i][j]);
-			//이미 탐색했거나, 배추가 있는 곳이 아니면 continue 
-			if(visited[i][j] || map[i][j] != 1) {
-//				System.out.println("패스!~~~");
-				continue;
-			}//if
-			//현재위치 방문!
-			visited[i][j] = true;
-			
-//				System.out.println("visited>>"+visited[i][j]);
-			Queue<int[]> q = new LinkedList<int[]>();
-			q.add(new int[] {i,j});
-			//아래와 같이 하면 오류남
-			//Queue<Integer> q = new LinkedList<Integer>();
-			//q.add(i);
-			//q.add(j);
-			count++;//여기서 값 증가 시켜주고 while문으로 연결된 모든 배추를 true로 바꿔준다...
-			while(!q.isEmpty()) {
-				int qi = q.peek()[0];
-				int qj = q.peek()[1];
-				q.poll();
-				visited[qi][qj]=true;
-//				System.out.println("qi>>"+qi);
-//				System.out.println("qj>>"+qj);
-				for(int p=0; p<4; p++) {
-						int ny = qi + dy[p];
-						int nx = qj + dx[p];
-						//범위를 벗어나면 continue 또는 배추가 있는곳이 아니면!
-						if(ny<0 || ny>=m || nx<0 || nx>=n || map[ny][nx] != 1) {
-							continue;
-						}
-						//이미 방문했으면 (true면) continue
-						if(visited[ny][nx]) {
-//							System.out.println("이미 true");
-							continue;
-						}//if
-						visited[ny][nx]=true;
-						q.add(new int[] {ny,nx});
-//						System.out.println("count>>====== "+count+" nynx: "+ny+nx);
-					}//p for
-				}//while
-			}//for
-		}//for
-		sb.append(count).append("\n");
-	}//private
-		
-	
 }
--- first_project/src/codingTest/codingTest5.java
+++ first_project/src/codingTest/codingTest5.java
@@ -7,73 +7,85 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 //문제
-//영우는 개구리다 개굴개굴개굴
-//
-//영우는 지금 n개의 돌이 일렬로 놓여있는 돌다리에 있다. 그리고 돌다리의 돌에는 숫자가 하나씩 적혀있다. 영우는 이 숫자가 적혀있는 만큼 왼쪽이나 오른쪽으로 점프할 수 있는데, 이때 돌다리 밖으로 나갈 수는 없다.
-//
-//영우는 이 돌다리에서 자기가 방문 가능한 돌들의 개수를 알고 싶어한다. 방문 가능하다는 것은 현재위치에서 다른 돌을 적절히 밟아 해당하는 위치로 이동이 가능하다는 뜻이다.
-//
-//현재 위치가 주어졌을 때, 영우가 방문 가능한 돌들의 개수를 출력하시오.
-//
+//섬의 개수
 //입력
-//첫 번째 줄에는 돌다리의 돌 개수 n이 주어진다.(1≤n≤100,000) 돌의 번호는 왼쪽부터 1번에서 n번이다. 다음 줄에는 그 위치에서 점프할 수 있는 거리 Ai가 주어진다.(1≤Ai≤100,000)
+//입력은 여러 개의 테스트 케이스로 이루어져 있다. 각 테스트 케이스의 첫째 줄에는 지도의 너비 w와 높이 h가 주어진다. w와 h는 50보다 작거나 같은 양의 정수이다.
 //
-//다음 줄에는 출발점 s가 주어진다.(1≤s≤n)
+//둘째 줄부터 h개 줄에는 지도가 주어진다. 1은 땅, 0은 바다이다.
+//
+//입력의 마지막 줄에는 0이 두 개 주어진다.
 //
 //출력
-//영우가 방문 가능한 돌들의 개수를 출력하시오.
+//각 테스트 케이스에 대해서, 섬의 개수를 출력한다.
 
 public class codingTest5 {
-	static int t,s; 
-	static int[] map;
-	static boolean[] visited;
-	static int[] dx = {-1,1};//좌우로만 이동
+	static int w,h;//맵의 가로 세로
+	static int[][] map;
+	static boolean[][] visited;
+	static int[] dx = {0,1,0,-1,1,1,-1,-1};
+	static int[] dy = {1,0,-1,0,1,-1,1,-1};
+	//탐색할 함수
+	private static void bfs(int i, int j) {
+//		System.out.println("ij>> "+i+" "+j);
+		//현재 위치 true
+		visited[i][j]=true;
+		Queue<int[]> q = new LinkedList<int[]>();
+		q.add(new int[] {i,j});
+		while (!q.isEmpty()) {
+			int y = q.peek()[0];
+			int x = q.peek()[1];
+			q.poll();
+			for(int l=0; l<8; l++) {
+				//대각선까지 탐색
+				int ny = y + dy[l];
+				int nx = x + dx[l];
+//				System.out.println("ny nx>> "+ny+" "+nx);
+				//범위벗어나거나 이미 true이면 패스
+				if(ny<0 || ny>=h || nx<0 || nx>=w || visited[ny][nx] || map[ny][nx] == 0) {
+//					System.out.println(ny+" "+nx+"continue!");
+					continue;
+				}
+				
+				//방문 갱신
+				visited[ny][nx]=true;
+				//큐에 값 넣기
+				q.add(new int[] {ny,nx});
+			}
+		}//while
+	}//private
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br=  new BufferedReader(new InputStreamReader(System.in));
-		t = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		s = Integer.parseInt(br.readLine());//출발점
-		
-		map = new int[t+1];
-		visited = new boolean[t+1];
-		
-		for(int i=1; i<=t; i++) {
-			int one = Integer.parseInt(st.nextToken());
-			map[i] = one;
-		}//for
-		
-		//시작위치 true
-		visited[s] = true;
-		Queue<Integer> q = new LinkedList<Integer>();
-		//q 초기값 입력
-		q.add(s);
-		while (!q.isEmpty()) {
-			//개구리의 현재 위치
-			int qone = q.poll();
-//			System.out.println("qone 개구리 위치>> "+qone);
-			//왼쪽과 오른쪽으로 이동
-			for(int j=0; j<2; j++) {
-				int x1 = qone + dx[j]*map[qone];
-				//범위를 벗어나거나 이미 true인 경우 continue
-				if(x1<1 || x1>t || visited[x1]) {
-					continue;
-				}//if
-//				System.out.println("x1해당위치에서 이동할 수 있는 인덱스>> "+x1);
-				//간적없는 갈 수 있는 범위이면 true로 변경
-				visited[x1]=true;
-				//개구리 위치 이동
-				q.add(x1);
-			}//for
-		}//while
-		
-		int count = 0;
-		for(int l=0; l<visited.length; l++) {
-			if(visited[l]==true) {
-//				System.out.println("count세기 l>> " + l);
-				count++;
+		while(true) {
+			StringTokenizer st1 = new StringTokenizer(br.readLine());
+			w = Integer.parseInt(st1.nextToken());//너비
+			h = Integer.parseInt(st1.nextToken());//높이
+			if(w==0 && h==0) break;// 0 0이 입력되면 break
+			
+			map = new int[h][w];
+			visited = new boolean[h][w];
+			//map입력
+			for(int i=0; i<h; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine()," ");
+				for(int j=0; j<w; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken()); 
+				}
 			}
-		}//for
+		
+			//함수선언
+			int count = 0;
+			for(int i=0; i<h; i++) {
+				for(int j=0; j<w; j++) {
+					//이미 true거나 바다이면 패스
+					if(visited[i][j] || map[i][j] == 0) {
+						continue;
+					}//if
+					bfs(i,j);
+					count++;
+				}
+			}
 		System.out.println(count);
-	}
+		}//while
+	}//public
 
 }
