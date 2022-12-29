bf62e593a5c0a9daf05b634817ae296fb037fbaf
OJH <aaa779979@naver.com>
2022-12-29 13:15:03 +0900

    codingTest

--- first_project/src/codingTest/codingTest5.java
+++ first_project/src/codingTest/codingTest5.java
@@ -7,85 +7,88 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 //문제
-//섬의 개수
+//침투
 //입력
-//입력은 여러 개의 테스트 케이스로 이루어져 있다. 각 테스트 케이스의 첫째 줄에는 지도의 너비 w와 높이 h가 주어진다. w와 h는 50보다 작거나 같은 양의 정수이다.
-//
-//둘째 줄부터 h개 줄에는 지도가 주어진다. 1은 땅, 0은 바다이다.
-//
-//입력의 마지막 줄에는 0이 두 개 주어진다.
+//첫째 줄에는 격자의 크기를 나타내는  M (2 ≤ M ≤ 1,000) 과 N (2 ≤ N ≤ 1,000) 이 주어진다. M줄에 걸쳐서, N개의 0 또는 1 이 공백 없이 주어진다. 0은 전류가 잘 통하는 흰색, 1은 전류가 통하지 않는 검은색 격자임을 뜻한다.
 //
 //출력
-//각 테스트 케이스에 대해서, 섬의 개수를 출력한다.
+//바깥에서 흘려준 전류가 안쪽까지 잘 전달되면 YES를 출력한다.
+//
+//그렇지 않으면 NO를 출력한다.
 
 public class codingTest5 {
-	static int w,h;//맵의 가로 세로
 	static int[][] map;
 	static boolean[][] visited;
-	static int[] dx = {0,1,0,-1,1,1,-1,-1};
-	static int[] dy = {1,0,-1,0,1,-1,1,-1};
-	//탐색할 함수
-	private static void bfs(int i, int j) {
-//		System.out.println("ij>> "+i+" "+j);
-		//현재 위치 true
-		visited[i][j]=true;
-		Queue<int[]> q = new LinkedList<int[]>();
-		q.add(new int[] {i,j});
+	static int n, m;//m이 길이 n이 너비
+	static int[] dx = {0,1,0,-1};
+	static int[] dy = {1,0,-1,0};
+	
+	private static void bfs(int j) {
+ 		Queue<int[]> q = new LinkedList<int[]>();
+		//초기값 입력
+		q.add(new int[] {0,j});
+		visited[0][j] = true;
 		while (!q.isEmpty()) {
 			int y = q.peek()[0];
 			int x = q.peek()[1];
 			q.poll();
-			for(int l=0; l<8; l++) {
-				//대각선까지 탐색
-				int ny = y + dy[l];
-				int nx = x + dx[l];
-//				System.out.println("ny nx>> "+ny+" "+nx);
-				//범위벗어나거나 이미 true이면 패스
-				if(ny<0 || ny>=h || nx<0 || nx>=w || visited[ny][nx] || map[ny][nx] == 0) {
-//					System.out.println(ny+" "+nx+"continue!");
+//			System.out.println("yx>> "+y+x);
+			for(int k=0; k<4; k++) {
+				int ny = y + dy[k];
+				int nx = x + dx[k];
+				//범위를 벗어난다면 continue
+				if(ny<0 || ny>=m || nx<0 || nx>=n || visited[ny][nx] || map[ny][nx] != 0) {
+//					System.out.println("continue!!!nynx>> "+ny+" "+nx);
 					continue;
-				}
+				}//if
 				
-				//방문 갱신
+				//갱신
 				visited[ny][nx]=true;
-				//큐에 값 넣기
-				q.add(new int[] {ny,nx});
-			}
+//				System.out.println("nynx>>>add "+ny+" "+nx);
+				q.add(new int[] {ny, nx});
+			}//for
 		}//while
 	}//private
-	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br=  new BufferedReader(new InputStreamReader(System.in));
-		while(true) {
-			StringTokenizer st1 = new StringTokenizer(br.readLine());
-			w = Integer.parseInt(st1.nextToken());//너비
-			h = Integer.parseInt(st1.nextToken());//높이
-			if(w==0 && h==0) break;// 0 0이 입력되면 break
-			
-			map = new int[h][w];
-			visited = new boolean[h][w];
-			//map입력
-			for(int i=0; i<h; i++) {
-				StringTokenizer st = new StringTokenizer(br.readLine()," ");
-				for(int j=0; j<w; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken()); 
-				}
-			}
+		StringTokenizer st1 = new StringTokenizer(br.readLine()," ");
+		m = Integer.parseInt(st1.nextToken());//길이
+		n = Integer.parseInt(st1.nextToken());//너비
 		
-			//함수선언
-			int count = 0;
-			for(int i=0; i<h; i++) {
-				for(int j=0; j<w; j++) {
-					//이미 true거나 바다이면 패스
-					if(visited[i][j] || map[i][j] == 0) {
-						continue;
-					}//if
-					bfs(i,j);
-					count++;
-				}
+		map = new int [m][n];
+		visited = new boolean [m][n];
+		
+		for(int i=0; i<m; i++) {
+//			StringTokenizer st = new StringTokenizer(br.readLine());
+			String s = br.readLine();
+			for(int j=0; j<n; j++) {
+				map[i][j]= (int)(s.charAt(j)-'0');//전류가 지나갈 수 있는 곳
+//				System.out.println(">>" + map[i][j]);
 			}
-		System.out.println(count);
-		}//while
+		}//for
+	
+		String answer = "";
+		for(int j=0; j<n; j++) {
+			if(answer.equals("YES")) {
+				System.out.println(answer);
+				break;
+				
+			}
+			if(!visited[0][j] && map[0][j]==0) {
+				bfs(j);
+			}//if
+			//outside쪽에 나갈 수 있는 곳이 없다면
+			
+			//탈출했으면yes출력
+			for (int l =0; l<n; l++) {
+				if(visited[m-1][l] && map[m-1][l]==0) {
+					answer = "YES";
+				}
+			}//for
+	}//for
+		if(!answer.equals("YES")) {
+			System.out.println("NO");
+		}
 	}//public
 
 }
