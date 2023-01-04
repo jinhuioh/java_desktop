ffce9ddb0ca6598bb67c014e40d65f585b9c86c3
OJH <aaa779979@naver.com>
2023-01-04 13:39:50 +0900

    codingTest

--- first_project/src/codingTest/codingTest.java
+++ first_project/src/codingTest/codingTest.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.Buffer;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -16,53 +17,36 @@ public class codingTest {
 	//static=전역 , final=최종적인, 즉 수정불가능한 변수.
 	//static final=상수
 
+	//상자 쌓기
+//	8
+//	1 6 2 5 7 3 5 6
 	public static void main(String[] args) throws NumberFormatException, IOException, InterruptedException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		//탐색했는지 판단
-		boolean[] visited = new boolean[n+1];
-		//이중리스트만들기
-		List<ArrayList<Integer>> map = new ArrayList<>();
-		for(int j = 0; j<=n; j++) {
-			map.add(new ArrayList<>());
-		}
 		
-		for(int i = 0; i<m; i++) {
-			StringTokenizer st1 = new StringTokenizer(br.readLine());
-			int p1 = Integer.parseInt(st1.nextToken());
-			int p2 = Integer.parseInt(st1.nextToken());
-			map.get(p1).add(p2);
-			map.get(p2).add(p1);
+		int[] map = new int[n];
+		
+		for(int i = 0; i<n; i++) {
+			map[i] = Integer.parseInt(st.nextToken());
 		}//for
-		//정답변수
+
+		//탐색시작
+		int[] dp = new int[n];
+		//dp배열의 모든 값을 1로 초기화
+		Arrays.fill(dp, 1);
 		int answer = 0;
-		Queue<Integer> q = new LinkedList<Integer>();
-		for(int k=1; k<=n; k++) {
-			//방문한 적 있으면 패스
-			if(visited[k]) {
-				continue;
-			}
-			//숫자 1부터 탐색
-			q.add(k);
-			//1 방문 표시
-			visited[k] = true;
-			//정답 변수 증가
-			answer ++;
-				while(!q.isEmpty()) {
-					//시작 숫자
-					int qp = q.poll();
-					for(int num: map.get(qp)) {//qp와 연결되어있는 숫자들
-						//이미 방문한 곳이면 continue
-						if(visited[num]) {
-							continue;
-						}
-						//qp와 연결된 모든 숫자 방문 표시
-						visited[num]=true;
-						q.add(num);
-					}
-				}//while
+		
+		for(int i = 0; i<n; i++) {
+			for(int j=0; j<i; j++) {
+				if(map[i]> map[j]) {//현재값보다 이전값이 더 크면 연산 안하므로 패스
+					//지금 위치값 vs 더 작은거 까지 계산한거에+1
+					System.out.println("i>> "+i+" j>> "+j+" dpi>> "+dp[i]+" dp[j]+1>> "+(dp[j]+1));
+					dp[i] = Math.max(dp[i], dp[j]+1);
+					
+				}
+			}//for
+			answer = Math.max(answer, dp[i]);
 		}//for
 		System.out.println(answer);
 	}
--- first_project/src/codingTest/codingTest3.java
+++ first_project/src/codingTest/codingTest3.java
@@ -1,62 +1,93 @@
 package codingTest;
 
+import java.util.List;
+import java.util.Queue;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
-//트리의 부모 찾기
-//문제
-//루트 없는 트리가 주어진다. 이때, 트리의 루트를 1이라고 정했을 때, 각 노드의 부모를 구하는 프로그램을 작성하시오.
+
+import javax.management.Query;
+import javax.swing.JPopupMenu.Separator;
 //
-//입력
-//첫째 줄에 노드의 개수 N (2 ≤ N ≤ 100,000)이 주어진다. 둘째 줄부터 N-1개의 줄에 트리 상에서 연결된 두 정점이 주어진다.
-//
-//출력
-//첫째 줄부터 N-1개의 줄에 각 노드의 부모 노드 번호를 2번 노드부터 순서대로 출력한다.
 public class codingTest3 {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		
-		ArrayList<ArrayList<Integer>> map = new ArrayList<>();
-		for(int i=0; i<=n; i++) {
-			map.add(new ArrayList<>());
-		}
-		for(int i1=1; i1<n; i1++) { 	
-			StringTokenizer st = new StringTokenizer(br.readLine()," ");
-			int p = Integer.parseInt(st.nextToken());//부모
-			int k = Integer.parseInt(st.nextToken());//자식
-			map.get(p).add(k);
-			map.get(k).add(p);
-			System.out.println(map);
-		}//for
-		
-		boolean[] visited = new boolean[n+1];//방문했는지 채크
-		int[] p = new int[n+1];//부모 노드 저장//정답이 되는 배열
-	
-		//bfs
-//		1번부터 시작
-		Queue<Integer> q = new LinkedList<Integer>();
-		q.add(1);
-		while(!q.isEmpty()) {
-			int v = q.poll();
-			for(int nums: map.get(v)) {// v번째 인덱스의 자식을 nums변수에 하나씩 넣어줌
-				if(!visited[nums]) {//자식을 방문한 적 없으면
-					visited[nums]=true;
-					//부모 지정
-					p[nums] = v;
-					//다음 자식으로 이동
-					q.add(nums);
-				}
-			}
-		}//while
-		for(int i2=2; i2<=n; i2++) {
-			System.out.println(p[i2]);
-		}
-		
-	}//public
+   public static void main(String[] args) throws IOException {
+
+   //거리가 k이하인 트리 노드에서 사과 수확하기
+      BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+      StringTokenizer st = new StringTokenizer(br.readLine()," ");
+      int n = Integer.parseInt(st.nextToken());
+      int k = Integer.parseInt(st.nextToken());
+      
+      List<ArrayList<Integer>> map = new ArrayList<ArrayList<Integer>>();
+      for(int j=0; j<n; j++) {
+    	  map.add(new ArrayList<Integer>());
+      }
+      for(int i=0; i<n-1; i++) {
+    	 StringTokenizer st1 = new StringTokenizer(br.readLine()," ");
+         int p1 = Integer.parseInt(st1.nextToken());
+         int p2 = Integer.parseInt(st1.nextToken());
+         map.get(p1).add(p2);//부모에 연결된 자식 넣기
+      }//for
+      
+      //사과 개수 입력
+      StringTokenizer st2 = new StringTokenizer(br.readLine()," ");
+      int[] arr = new int[n];
+      for(int i=0; i<n; i++) {
+    	  arr[i] = Integer.parseInt(st2.nextToken());
+      }//for
+      
+
+      
+      int index = 0;
+      Queue<Integer> q1 = new LinkedList<Integer>();
+      q1.add(0);
+      //해당 자식의 부모를 넣기
+      int[] indexarr = new int[n];
+      indexarr[0] = 0;
+      for(int i=0; i<n; i++) {
+    	  int q1poll = q1.poll();
+    	  for(int nums: map.get(q1poll)) {
+    		  indexarr[nums] = i;
+    		  System.out.println(nums+" 번째 indexarr의 값은>> "+i);
+    		  q1.add(nums);
+    	  }
+      }//for
+      //방문했는지 채크
+      boolean[] visited = new boolean[n];
+      
+      //0부터 탐색 시작!!!
+      int answer = arr[0];//사과 개수의 초기값 0번째 루트부터 센다.
+      Queue<Integer> q = new LinkedList<Integer>();
+      q.add(0);
+      visited[0] = true;
+      
+      //k번만큼 for문 돌림
+      while(index<=k) {
+    	  index++;
+    	  int qpoll = q.poll();
+    	  visited[qpoll] = true;
+    	  for(int num: map.get(qpoll)) {//현재 위치에 담긴 숫자들
+    		  if(visited[num]) {
+    			  continue;
+    		  }
+    		  //사과 개수 세기
+    		  answer += arr[num];
+    		  visited[num] = true;
+    		  System.out.println("index>> "+index);
+    		  System.out.println("num>> "+num);
+    		  System.out.println("answer>> "+answer);
+    		  q.add(num);
+    	  }//for
+      }//for
+      System.out.println(answer);
+   }
+
 }
+   
