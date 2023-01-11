49e2c2118290e669ba27796303fad37ec74ddcb8
OJH <aaa779979@naver.com>
2023-01-11 22:32:11 +0900

    codingTest

--- first_project/src/codingTest/codingTest3.java
+++ first_project/src/codingTest/codingTest3.java
@@ -15,90 +15,55 @@ import java.util.StringTokenizer;
 
 import javax.management.Query;
 import javax.swing.JPopupMenu.Separator;
-//적록색약
-//입력
-//첫째 줄에 N이 주어진다. (1 ≤ N ≤ 100)
-//
-//둘째 줄부터 N개 줄에는 그림이 주어진다.
+///입력
+//첫째 줄에 테스트 케이스의 개수 T가 주어진다. 각 테스트 케이스의 첫째 줄에는 n (1 ≤ n ≤ 100,000)이 주어진다. 
+//다음 두 줄에는 n개의 정수가 주어지며, 각 정수는 그 위치에 해당하는 스티커의 점수이다. 연속하는 두 정수 사이에는 빈 칸이 하나 있다. 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다. 
 //
 //출력
-//적록색약이 아닌 사람이 봤을 때의 구역의 개수와 적록색약인 사람이 봤을 때의 구역의 수를 공백으로 구분해 출력한다.
-//
+//각 테스트 케이스 마다, 2n개의 스티커 중에서 두 변을 공유하지 않는 스티커 점수의 최댓값을 출력한다.
 //예제 입력 1 
+//2
 //5
-//RRRBB
-//GGBBB
-//BBBRR
-//BBRRR
-//RRRRR
+//50 10 100 20 40
+//30 50 70  10 60
+//7
+//10 30 10 50 100 20 40
+//20 40 30 50 60 20 80
 //예제 출력 1 
-//4 3
+//260
+//290
+
 public class codingTest3 {
-	static int n;
-	static char[][] map1, map2;//입력받은 그래프 넣을 배열
-	static boolean[][] visited1,visited2;//방문했는지 채크할 배열
-	static int[] dx = {0,1,0,-1};
-	static int[] dy = {1,0,-1,0};
-	
-	private static void bfs(int i, int j, char[][] map, boolean[][] visited) {
-		char word = map[i][j];
-		Queue<int[]> q = new LinkedList<int[]>();
-		q.add(new int[] {i,j});
-		visited[i][j] = true;
-		while (!q.isEmpty()) {
-			int y = q.peek()[0];
-			int x = q.peek()[1];
-			q.poll();
-			for(int k = 0; k<4; k++) {
-				int ny = y+ dy[k];
-				int nx = x+ dx[k];
-				if(ny<0 || ny>=n || nx<0 || nx>=n ) continue;
-				if(!visited[ny][nx] && map[ny][nx] == word) {
-					visited[ny][nx] = true;
-					q.add(new int[] {ny, nx});
-				}
-			}
-		}
-	}
-	
+	static int n,m;
+	static int[][] map,dp;
     public static void main(String[] args) throws IOException {
 	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	   n = Integer.parseInt(br.readLine());
-	   map1 = new char[n][n];
-	   map2 = new char[n][n];
-	   visited1 = new boolean[n][n];
-	   visited2 = new boolean[n][n];
+	  
 	   for(int i = 0; i<n; i++) {
-		   String s = br.readLine();
-		   for(int j = 0; j<n; j++) {
-			   char one = s.charAt(j);
-			   map1[i][j] = one;
-			   map2[i][j] = one;
-			   if(one =='G') {
-				   map2[i][j] = 'R';
-			   }
-		   }//for
-	   }//for
-	   
 		   
-	   int count1 = 0;//적록색약아님
-	   int count2 = 0;//적록색약
-	   for(int i = 0; i<n; i++) {
-		   for(int j = 0; j<n; j++) {
-			   if(visited1[i][j]) continue;
-			   count1++;
-			   bfs(i, j, map1, visited1);
+		   m = Integer.parseInt(br.readLine());
+		   map = new int[2][m+1];
+		   dp = new int[2][m+1];
+		   
+		   for(int j = 0; j<2; j++) {
+			   StringTokenizer st = new StringTokenizer(br.readLine());
+			   for(int k = 1; k<=m; k++) {
+				   map[j][k] = Integer.parseInt(st.nextToken());
+			   }//for
+		   }//for
+		   
+		   //최대값 구하기!
+		   dp[0][1] = map[0][1]; 
+		   dp[1][1] = map[1][1]; 
+		   //2부터
+		   for(int j = 2; j<=m; j++) {
+			  dp[0][j] = Math.max(dp[1][j-1], dp[1][j-2]) + map[0][j]; 
+			  dp[1][j] = Math.max(dp[0][j-1], dp[0][j-2]) + map[1][j]; 
+
 		   }
-	   }
-	   //적록색약이면 빨강 == 파랑
-	   for(int i = 0; i<n; i++) {
-		   for(int j = 0; j<n; j++) {
-			   if(visited2[i][j]) continue;
-			   count2++;
-			   bfs(i, j, map2, visited2);
-		   }
-	   }//for
-	   System.out.println(count1+" "+count2);
+		   System.out.println(Math.max(dp[0][m], dp[1][m]));
+	   }//n for
     }
 
 }
