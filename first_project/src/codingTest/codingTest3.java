0d61e9e6b782727b3405870571f8d1892e696581
OJH <aaa779979@naver.com>
2023-01-15 20:53:25 +0900

    codingTest

--- first_project/src/codingTest/codingTest3.java
+++ first_project/src/codingTest/codingTest3.java
@@ -15,56 +15,28 @@ import java.util.StringTokenizer;
 
 import javax.management.Query;
 import javax.swing.JPopupMenu.Separator;
-///입력
-//첫째 줄에 테스트 케이스의 개수 T가 주어진다. 각 테스트 케이스의 첫째 줄에는 n (1 ≤ n ≤ 100,000)이 주어진다. 
-//다음 두 줄에는 n개의 정수가 주어지며, 각 정수는 그 위치에 해당하는 스티커의 점수이다. 연속하는 두 정수 사이에는 빈 칸이 하나 있다. 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다. 
+//정수삼각형
+//입력
+//첫째 줄에 삼각형의 크기 n(1 ≤ n ≤ 500)이 주어지고, 둘째 줄부터 n+1번째 줄까지 정수 삼각형이 주어진다.
 //
 //출력
-//각 테스트 케이스 마다, 2n개의 스티커 중에서 두 변을 공유하지 않는 스티커 점수의 최댓값을 출력한다.
+//첫째 줄에 합이 최대가 되는 경로에 있는 수의 합을 출력한다.
+//
 //예제 입력 1 
-//2
 //5
-//50 10 100 20 40
-//30 50 70  10 60
 //7
-//10 30 10 50 100 20 40
-//20 40 30 50 60 20 80
+//3 8
+//8 1 0
+//2 7 4 4
+//4 5 2 6 5
 //예제 출력 1 
-//260
-//290
-
+//30
 public class codingTest3 {
-	static int n,m;
-	static int[][] map,dp;
+	static int n;
     public static void main(String[] args) throws IOException {
 	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	   n = Integer.parseInt(br.readLine());
-	  
-	   for(int i = 0; i<n; i++) {
-		   
-		   m = Integer.parseInt(br.readLine());
-		   map = new int[2][m+1];
-		   dp = new int[2][m+1];
-		   
-		   for(int j = 0; j<2; j++) {
-			   StringTokenizer st = new StringTokenizer(br.readLine());
-			   for(int k = 1; k<=m; k++) {
-				   map[j][k] = Integer.parseInt(st.nextToken());
-			   }//for
-		   }//for
-		   
-		   //최대값 구하기!
-		   dp[0][1] = map[0][1]; 
-		   dp[1][1] = map[1][1]; 
-		   //2부터
-		   for(int j = 2; j<=m; j++) {
-			  dp[0][j] = Math.max(dp[1][j-1], dp[1][j-2]) + map[0][j]; 
-			  dp[1][j] = Math.max(dp[0][j-1], dp[0][j-2]) + map[1][j]; 
-
-		   }
-		   System.out.println(Math.max(dp[0][m], dp[1][m]));
-	   }//n for
+	
     }
-
 }
    
