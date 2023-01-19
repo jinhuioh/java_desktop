7401dfd6d9725dc162ca34963d09dda42f252f30
OJH <aaa779979@naver.com>
2023-01-19 21:38:12 +0900

    codingTest

--- first_project/src/codingTest/codingTest3.java
+++ first_project/src/codingTest/codingTest3.java
@@ -1,5 +1,42 @@
 package codingTest;
-
+//문제 : 상범빌딩
+//입력
+//입력은 여러 개의 테스트 케이스로 이루어지며, 각 테스트 케이스는 세 개의 정수 L, R, C로 시작한다. L(1 ≤ L ≤ 30)은 상범 빌딩의 층 수이다. R(1 ≤ R ≤ 30)과 C(1 ≤ C ≤ 30)는 상범 빌딩의 한 층의 행과 열의 개수를 나타낸다.
+//
+//그 다음 각 줄이 C개의 문자로 이루어진 R개의 행이 L번 주어진다. 각 문자는 상범 빌딩의 한 칸을 나타낸다. 금으로 막혀있어 지나갈 수 없는 칸은 '#'으로 표현되고, 비어있는 칸은 '.'으로 표현된다. 당신의 시작 지점은 'S'로 표현되고, 탈출할 수 있는 출구는 'E'로 표현된다. 각 층 사이에는 빈 줄이 있으며, 입력의 끝은 L, R, C가 모두 0으로 표현된다. 시작 지점과 출구는 항상 하나만 있다.
+//
+//출력
+//각 빌딩에 대해 한 줄씩 답을 출력한다. 만약 당신이 탈출할 수 있다면 다음과 같이 출력한다.
+//
+//Escaped in x minute(s).
+//여기서 x는 상범 빌딩을 탈출하는 데에 필요한 최단 시간이다.
+//
+//만일 탈출이 불가능하다면, 다음과 같이 출력한다.
+//
+//Trapped!
+//예제 입력 1 
+//3 4 5
+//S....
+//.###.
+//.##..
+//###.#
+//
+//#####
+//#####
+//##.##
+//##...
+//
+//#####
+//#####
+//#.###
+//####E
+//
+//1 3 3
+//S##
+//#E#
+//###
+//
+//0 0 0
 import java.util.List;
 import java.util.Queue;
 import java.io.BufferedReader;
@@ -16,60 +53,104 @@ import java.util.StringTokenizer;
 import javax.management.Query;
 import javax.swing.JPopupMenu.Separator;
 public class codingTest3 {
-//	RGB거리
-//	문제
-//	RGB거리에는 집이 N개 있다. 거리는 선분으로 나타낼 수 있고, 1번 집부터 N번 집이 순서대로 있다.
-//
-//	집은 빨강, 초록, 파랑 중 하나의 색으로 칠해야 한다. 각각의 집을 빨강, 초록, 파랑으로 칠하는 비용이 주어졌을 때, 아래 규칙을 만족하면서 모든 집을 칠하는 비용의 최솟값을 구해보자.
-//
-//	1번 집의 색은 2번 집의 색과 같지 않아야 한다.
-//	N번 집의 색은 N-1번 집의 색과 같지 않아야 한다.
-//	i(2 ≤ i ≤ N-1)번 집의 색은 i-1번, i+1번 집의 색과 같지 않아야 한다.
-//	입력
-//	첫째 줄에 집의 수 N(2 ≤ N ≤ 1,000)이 주어진다. 둘째 줄부터 N개의 줄에는 각 집을 빨강, 초록, 파랑으로 칠하는 비용이 1번 집부터 한 줄에 하나씩 주어진다. 집을 칠하는 비용은 1,000보다 작거나 같은 자연수이다.
-//
-//	출력
-//	첫째 줄에 모든 집을 칠하는 비용의 최솟값을 출력한다.
-//
-//	예제 입력 1 
-//	3
-//	26 40 83
-//	49 60 57
-//	13 89 99
-//	예제 출력 1 
-//	96
+	static int l,r,c;//층수,행,열
+	static char[][][] map;
+	static int[][][] arr;
+	static boolean[][][] visited;
+	static int[] dz = {-1,1,0,0,0,0};
+	static int[] dy = {0,0,1,-1,0,0};
+	static int[] dx = {0,0,0,0,-1,1};
 	
-	//접근법: 2번째 줄부터 각 숫자의 최소값으로 위에꺼랑 더해서 갱신해나감
-	static int n;
-	static int[][] dp;
+	private static int bfs(int i, int j, int k) {
+		int answer = 0;
+		int end = 0;
+		visited[i][j][k] = true;
+		Queue<int[]> q = new LinkedList<int[]>();
+		q.add(new int[] {i,j,k});
+		Loop1 :
+		while (!q.isEmpty()) {
+			int z = q.peek()[0];
+		    int y = q.peek()[1];
+	        int x = q.peek()[2];
+	        q.poll();
+			for(int i1 = 0; i1<6; i1++) {
+				int nz = z + dz[i1];
+				int ny = y + dy[i1];
+				int nx = x + dx[i1];
+				
+				if(nz>=0 && nz<l && ny>=0 && ny<r && nx>=0 && nx<c && map[nz][ny][nx]=='E') {
+					arr[nz][ny][nx] = arr[z][y][x] + 1;//값 갱신
+					answer = arr[nz][ny][nx];
+//					System.out.println("nzyx>> "+nz+" "+ny+" "+nx+"여기방문..");
+					end = 1;
+					break Loop1;
+				}
+				if(nz>=0 && nz<l && ny>=0 && ny<r && nx>=0 && nx<c && !visited[nz][ny][nx] && map[nz][ny][nx]=='.') {
+					q.add(new int[] {nz,ny,nx});
+					visited[nz][ny][nx] = true;
+//					System.out.println("zyx>> "+z+" "+y+" "+x);
+//					System.out.println("nzyx>> "+nz+" "+ny+" "+nx+"가 . 입니다");
+					arr[nz][ny][nx] = arr[z][y][x] + 1;
+//					answer = arr[nz][ny][nx];
+//					System.out.println(answer);
+				}//if
+			}//for
+		}//while
+		if(end!=1) {
+			answer = 0;
+		}
+		return answer;
+	}
+	
     public static void main(String[] args) throws IOException {
 	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	   n = Integer.parseInt(br.readLine());
-	   dp = new int[n][n];
-	   for(int i = 0; i<n; i++) {
+	  
+	   while (true) {
 		   StringTokenizer st = new StringTokenizer(br.readLine());
-		   for(int j = 0; j<3; j++) {
-			   dp[i][j] = Integer.parseInt(st.nextToken());
-		   }//for
-	   }//for
+		   if(st.countTokens()==0) {
+			   st = new StringTokenizer(br.readLine());
+		   }
+		   l = Integer.parseInt(st.nextToken());
+		   r = Integer.parseInt(st.nextToken());
+		   c = Integer.parseInt(st.nextToken());
 
-	   for(int i = 1; i<n; i++) {
-		   for(int j = 0; j<3; j++) {
-			   if(j==0) {
-				   dp[i][j] = Math.min(dp[i-1][j+1], dp[i-1][j+2])+dp[i][j];
-				   
-			   }
-			   else if(j==1) {
-				   dp[i][j] = Math.min(dp[i-1][j-1], dp[i-1][j+1])+dp[i][j];
-			   
-			   }
-			   else {
-				   dp[i][j] = Math.min(dp[i-1][j-1], dp[i-1][j-2])+dp[i][j];
-				   
-			   }
+		   if(l==0 && r==0 && c==0) {
+			   System.exit(0);
+		   }
+		   map = new char[l][r][c];
+		   arr = new int[l][r][c];
+		   visited = new boolean[l][r][c];
+		   //start부분을 넣을 큐
+		   Queue<int[]> q = new LinkedList<int[]>();
+		   for(int i = 0; i<l; i++) {
+			   for(int j = 0; j<r; j++) {
+				   String s = br.readLine();
+				   if(s.equals("")) {
+					   s = br.readLine();//한 줄 공백이 들어오면 다시 한 줄 입력 받음
+				   }
+				   for(int k = 0; k<c; k++) {
+					   map[i][j][k] = s.charAt(k);
+					   //만약 s가 입력되면 큐에 넣어준다.
+					   if(map[i][j][k]=='S') {
+						   q.add(new int[] {i,j,k});
+					   }
+				   }//for
+			   }//for
 		   }//for
-	   }//for
-	   System.out.println(Math.min(dp[n-1][0], Math.min(dp[n-1][1], dp[n-1][2])));
+		   
+		   //출구찾기
+		   int z = q.peek()[0];
+		   int y = q.peek()[1];
+		   int x = q.peek()[2];
+		   int answer = bfs(z,y,x);
+		   if(answer==0) {
+			   System.out.println("Trapped!");
+		   }
+		   else {
+			   System.out.println("Escaped in " +answer+ " minute(s).");
+		   }
+	   }//while
+	   
     }
 }
    
