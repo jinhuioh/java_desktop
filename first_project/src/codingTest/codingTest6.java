d19031577d41f019b59ff46f8b30575e0830d013
OJH <aaa779979@naver.com>
2023-01-28 23:36:16 +0900

    codingTest

--- first_project/src/codingTest/codingTest6.java
+++ first_project/src/codingTest/codingTest6.java
@@ -3,123 +3,75 @@ package codingTest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
 
+//			문제 설명
+//			압축
+//			신입사원 어피치는 카카오톡으로 전송되는 메시지를 압축하여 전송 효율을 높이는 업무를 맡게 되었다. 메시지를 압축하더라도 전달되는 정보가 바뀌어서는 안 되므로, 압축 전의 정보를 완벽하게 복원 가능한 무손실 압축 알고리즘을 구현하기로 했다.
+//
+//			어피치는 여러 압축 알고리즘 중에서 성능이 좋고 구현이 간단한 LZW(Lempel–Ziv–Welch) 압축을 구현하기로 했다. LZW 압축은 1983년 발표된 알고리즘으로, 이미지 파일 포맷인 GIF 등 다양한 응용에서 사용되었다.
+//
+//			LZW 압축은 다음 과정을 거친다.
+//
+//			길이가 1인 모든 단어를 포함하도록 사전을 초기화한다.
+//			사전에서 현재 입력과 일치하는 가장 긴 문자열 w를 찾는다.
+//			w에 해당하는 사전의 색인 번호를 출력하고, 입력에서 w를 제거한다.
+//			입력에서 처리되지 않은 다음 글자가 남아있다면(c), w+c에 해당하는 단어를 사전에 등록한다.
+//			단계 2로 돌아간다.
+//			압축 알고리즘이 영문 대문자만 처리한다고 할 때, 사전은 다음과 같이 초기화된다. 사전의 색인 번호는 정수값으로 주어지며, 1부터 시작한다고 하자.
+//
+//			색인 번호	1	2	3	...	24	25	26
+//			단어	A	B	C	...	X	Y	Z
+//			예를 들어 입력으로 KAKAO가 들어온다고 하자.
+//
+//			현재 사전에는 KAKAO의 첫 글자 K는 등록되어 있으나, 두 번째 글자까지인 KA는 없으므로, 첫 글자 K에 해당하는 색인 번호 11을 출력하고, 다음 글자인 A를 포함한 KA를 사전에 27 번째로 등록한다.
+//			두 번째 글자 A는 사전에 있으나, 세 번째 글자까지인 AK는 사전에 없으므로, A의 색인 번호 1을 출력하고, AK를 사전에 28 번째로 등록한다.
+//			세 번째 글자에서 시작하는 KA가 사전에 있으므로, KA에 해당하는 색인 번호 27을 출력하고, 다음 글자 O를 포함한 KAO를 29 번째로 등록한다.
+//			마지막으로 처리되지 않은 글자 O에 해당하는 색인 번호 15를 출력한다.
+//			현재 입력(w)	다음 글자(c)	출력	사전 추가(w+c)
+//			K	A	11	27: KA
+//			A	K	1	28: AK
+//			KA	O	27	29: KAO
+//			O		15	
+//			이 과정을 거쳐 다섯 글자의 문장 KAKAO가 4개의 색인 번호 [11, 1, 27, 15]로 압축된다.
+//
+//			입력으로 TOBEORNOTTOBEORTOBEORNOT가 들어오면 다음과 같이 압축이 진행된다.
+//
+//			현재 입력(w)	다음 글자(c)	출력	사전 추가(w+c)
+//			T	O	20	27: TO
+//			O	B	15	28: OB
+//			B	E	2	29: BE
+//			E	O	5	30: EO
+//			O	R	15	31: OR
+//			R	N	18	32: RN
+//			N	O	14	33: NO
+//			O	T	15	34: OT
+//			T	T	20	35: TT
+//			TO	B	27	36: TOB
+//			BE	O	29	37: BEO
+//			OR	T	31	38: ORT
+//			TOB	E	36	39: TOBE
+//			EO	R	30	40: EOR
+//			RN	O	32	41: RNO
+//			OT		34	
+//			입력 형식
+//			입력으로 영문 대문자로만 이뤄진 문자열 msg가 주어진다. msg의 길이는 1 글자 이상, 1000 글자 이하이다.
+//
+//			출력 형식
+//			주어진 문자열을 압축한 후의 사전 색인 번호를 배열로 출력하라.
+//
+//			입출력 예제
+//			msg	answer
+//			KAKAO	[11, 1, 27, 15]
+//			TOBEORNOTTOBEORTOBEORNOT	[20, 15, 2, 5, 15, 18, 14, 15, 20, 27, 29, 31, 36, 30, 32, 34]
+//			ABABABABABABABAB	[1, 2, 27, 29, 28, 31, 30]
 public class codingTest6 {
-//치즈 다른 답안(codingTest7은 내가 푼 방법.)
-	
-	    static int[][] map;
-	    static boolean[][] visited;
-	    static int n;
-	    static int m;
-	    static int[] mi = {0, 0, 1, -1};
-	    static int[] mj = {1, -1, 0, 0};
-	    static ArrayList<Loc> cheese_melt;
-
-	    static class Loc{
-	        int i;
-	        int j;
-
-	        public Loc(int i, int j) {
-	            this.i = i;
-	            this.j = j;
-	        }
-	    }
-
 		public static void main(String[] args) throws IOException {
-	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	        String[] inputs = br.readLine().split(" ");
-	        n = Integer.parseInt(inputs[0]);
-	        m = Integer.parseInt(inputs[1]);
-
-	        map = new int[n][m];
-
-	        for (int i = 0; i < n; i++) {
-	            inputs = br.readLine().split(" ");
-	            for (int j = 0; j < m; j++) {
-	                map[i][j] = Integer.parseInt(inputs[j]);
-	            }
-	        }
-
-	        int time = 0;
-
-	        cheese_melt = new ArrayList<>();
-
-	        while(true){
-	            cheese_melt.clear();
-	            visited = new boolean[n][m];
-
-	            setExternalAir(); // -1은 외부 공기, 0은 내부 공기
-
-	            for (int i = 1; i < n-1; i++) {
-	                for (int j = 1; j < m-1; j++) {
-	                    if(map[i][j]==1){ // 치즈이면
-	                        if (isMelt(i,j)) { // 1시간 내에 녹을 치즈면 == 두 변이 외부 공기와 맞닿아있으면
-	                            cheese_melt.add(new Loc(i, j));
-	                        }
-	                    }
-
-	                }
-	            }
-
-	            // 더 이상 녹을 치즈가 없다 == 치즈가 다 녹았다 -> 종료!
-	            if(cheese_melt.size()==0) break;	
-
-	            // 겉 치즈 녹음
-	            for (Loc l : cheese_melt) {
-	                map[l.i][l.j] = 0;
-	            }
-
-	            time ++;
-	        }
-
-	        System.out.println(time);
-	    }
-
-	    public static boolean isMelt(int i, int j){
-	        int cnt = 0;
-	        for (int d = 0; d < 4; d++) { // 상하좌우 확인
-	            int ni = i + mi[d];
-	            int nj = j + mj[d];
-
-	            if(ni<0 || nj<0 || ni>n || nj>m) continue;
-
-	            if(map[ni][nj] == -1) cnt++;
-	        }
 			
-	        // 두 면 이상 맞닿아있는지 true/false로 return
-	        return cnt >= 2;
-	    }
+		
+		}
 
-	    public static void setExternalAir(){
+}
 
-	        Queue<Loc> q = new LinkedList<>();
-
-	        q.add(new Loc(0, 0));
-	        map[0][0] = -1;
-
-	        while (!q.isEmpty()) {
-	            Loc now = q.poll();
-
-	            for (int d = 0; d < 4; d++) {
-	                int ni = now.i + mi[d];
-	                int nj = now.j + mj[d];
-
-	                if(ni<0 || nj<0 || ni>=n || nj>=m ) continue;
-
-	                if(visited[ni][nj] || map[ni][nj] == 1) continue;
-
-	                visited[ni][nj] = true;
-	                map[ni][nj] = -1; // 외부 공기는 -1로 초기화
-	                q.add(new Loc(ni, nj));
-
-	            }
-	        }
-	    }
-	}
--- first_project/src/codingTest/codingTest7.java
+++ first_project/src/codingTest/codingTest7.java
@@ -7,208 +7,63 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
-//치즈
-//	입력
-//	첫째 줄에는 사각형 모양 판의 세로와 가로의 길이가 양의 정수로 주어진다. 세로와 가로의 길이는 최대 100이다. 판의 각 가로줄의 모양이 윗 줄부터 차례로 둘째 줄부터 마지막 줄까지 주어진다. 
-//	치즈가 없는 칸은 0, 치즈가 있는 칸은 1로 주어지며 각 숫자 사이에는 빈칸이 하나씩 있다.
-//
-//	출력
-//	첫째 줄에는 치즈가 모두 녹아서 없어지는 데 걸리는 시간을 출력하고, 둘째 줄에는 모두 녹기 한 시간 전에 남아있는 치즈조각이 놓여 있는 칸의 개수를 출력한다.
-//	예제 입력 1 
-//	13 12
-//	0 0 0 0 0 0 0 0 0 0 0 0
-//	0 0 0 0 0 0 0 0 0 0 0 0
-//	0 0 0 0 0 0 0 1 1 0 0 0
-//	0 1 1 1 0 0 0 1 1 0 0 0
-//	0 1 1 1 1 1 1 0 0 0 0 0
-//	0 1 1 1 1 1 0 1 1 0 0 0
-//	0 1 1 1 1 0 0 1 1 0 0 0
-//	0 0 1 1 0 0 0 1 1 0 0 0
-//	0 0 1 1 1 1 1 1 1 0 0 0
-//	0 0 1 1 1 1 1 1 1 0 0 0
-//	0 0 1 1 1 1 1 1 1 0 0 0
-//	0 0 1 1 1 1 1 1 1 0 0 0
-//	0 0 0 0 0 0 0 0 0 0 0 0
-//	예제 출력 1 
-//	3
-//	5
-class ch{
-	int y;
-	int x;
-	ch(int y, int x){
-		this.y = y;
-		this.x = x;
-	}
-}
+
 
 public class codingTest7 {
-	static int m,n;
-	static int[][] map;
-	static int[][] arr;
-	static boolean[][] visited,visited1;
-	static int[] dx = {0,0,-1,1};
-	static int[] dy = {-1,1,0,0};
-	static Queue<ch> q = new LinkedList<ch>();
-	static Queue<ch> q1 = new LinkedList<ch>();
-	static Queue<ch> q2 = new LinkedList<ch>();
-	
-	//마지막에 있는 칸의 개수 반환
-	private static int bfs(int i , int j) {
-		int count;
-		for(int i1 = 0; i1<n; i1++) {
-			for(int j1 = 0; j1<m; j1++) {
-				
-			}
-		}
-		
-		
-		return 0;
-	}
-	
 	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		map = new int[n][m];
-		arr = new int[n][m];//치즈가 다 없어지는데 걸리는 일수 계산할 배열
-		visited = new boolean[n][m];
-		visited1 = new boolean[n][m];
-		//map입력받기
-		for(int i = 0; i<n; i++) {
-			StringTokenizer st1 = new StringTokenizer(br.readLine());
-			for(int j = 0; j<m; j++) {
-				map[i][j] = Integer.parseInt(st1.nextToken());
-				if(map[i][j]==1) {//0과 인접해있는 위치를 전부 큐에 넣는다.
-						if(map[i-1][j]==2 || map[i+1][j]==2 || map[i][j-1]==2 || map[i][j+1]==2) {
-							q1.add(new ch(i, j));
-					}
-				}
-			}
-		}//for
-		
-		//1. 1로 둘러싸인 0을 빼고 겉부분 0을 2로 표시
-		map[0][0]=2;
-		visited[0][0] = true;
-		q.add(new ch(0, 0));
-		while (!q.isEmpty()) {
-			ch qp = q.remove();
-			int y = qp.y; 
-			int x = qp.x;
-			
-			for(int k = 0; k<4; k++) {
-				int ny = y + dy[k];
-				int nx = x + dx[k];
-				if(ny>=0 && nx>=0 && ny<n && nx<m && map[ny][nx]==0 && !visited[ny][nx]) {
-					visited[ny][nx]=true;
-					map[ny][nx] = 2;
-					q.add(new ch(ny, nx));
-				}//if
-			}//for
-		}//while
-
-		
-//		for(int i = 0; i<n; i++) {
-//			for(int j = 0; j<m; j++) {
-//				System.out.print(map[i][j]);
-//			}
-//			System.out.println();
-//		}//for
-//		System.out.println();
-		int count = 0;
-		List<Integer> answer_list = new ArrayList<Integer>();
-		while (true) {
-			int count_one = 0;
-			int finish = 0;
-//		2. q1에 넣은 0과 인접한 위치 중에서 2와 인접한 것들만 연산
-			while (!q1.isEmpty()) {
-				ch qp1 = q1.remove();
-				int y1 = qp1.y;
-				int x1 = qp1.x;
-				
-//				visited1[y1][x1] = true;
-				//공기와 닿는다면 해당 위치 2로 갱신
-				map[y1][x1] = 2;
-				
-				
-				for(int i = 0; i<n; i++) {
-					for(int j = 0; j<m; j++) {
-	//				//for문 돌면서 0과 2가 인접해있으면 0을 2로 갱신
-						if(map[i][j]==0) {
-							if(map[i-1][j]==2 || map[i+1][j]==2 || map[i][j-1]==2 || map[i][j+1]==2) {
-							visited = new boolean[n][m];
-							q2.add(new ch(i,j));
-							visited[i][j] = true;
-							while (!q2.isEmpty()) {
-								ch qp2 = q2.remove();
-								int y2 = qp2.y; 
-								int x2 = qp2.x;
-								
-								for(int k = 0; k<4; k++) {
-									int ny = y2 + dy[k];
-									int nx = x2 + dx[k];
-										if(ny>=0 && nx>=0 && ny<n && nx<m && map[ny][nx]==0 && !visited[ny][nx]) {
-											visited[ny][nx]=true;
-											map[ny][nx] = 2;
-											q2.add(new ch(ny, nx));
-										}
-									}//if
-								}
-							}//for
-						}//if
-					}
-				}
-				
-//				for(int k = 0; k<4; k++) {
-//					int ny1 = y1 + dy[k];
-//					int nx1 = x1 + dx[k];
-//					System.out.println("nynx1>>> "+ny1+" "+nx1);
-//					if(ny1>=0 && nx1>=0 && ny1<n && nx1<m ) {
-//						if(map[ny1][nx1]==1 && !visited1[ny1][nx1]) {
-//							visited1[ny1][nx1]=true;
-//							map[ny1][nx1] = 2;
-//						}
-//					}//if
-//				}//for
-			}//while
-//		
-//			for(int i = 0; i<n; i++) {
-//				for(int j = 0; j<m; j++) {
-//					System.out.print(map[i][j]);
-//				}
-//				System.out.println();
-//			}//for
-//			System.out.println();
-//			System.out.println();
-			
-			for(int i = 0; i<n; i++) {
-				for(int j = 0; j<m; j++) {
-					
-					if(map[i][j]==1) {//0과 인접해있는 위치를 전부 큐에 넣는다.
-						count_one++;
-						if(map[i-1][j]==2 || map[i+1][j]==2 || map[i][j-1]==2 || map[i][j+1]==2) {
-							q1.add(new ch(i, j));
-							finish = 1;
-						}
-					}
-				}
-			}//for
-			answer_list.add(count_one);
-			if(finish == 0) {
-				break;
-			}
-			else if(finish == 1) {
-				count++;
-			}
-			
-			
-		}//while
-		System.out.println(count);
-		System.out.println(answer_list.get(answer_list.size()-2));
 			
 	}
 
+}
+
+//괄호문제
+class Solution1 {
+  public int solution(String s) {
+      int answer = 0;
+      String str=s;
+      
+      for(int i=0;i<s.length();i++){
+          char temp=str.charAt(0);
+          str=str.substring(1)+temp;
+          if(isCorrect(str)){
+              answer++;
+          }
+      }
+      
+      return answer;
+  }
+  
+  public boolean isCorrect (String str){
+      Stack<Character> s = new Stack<>();
+      
+      char[] arr = str.toCharArray();
+      
+      for(char data:arr){
+          if(s.isEmpty()){
+              s.push(data);
+          }
+          else if(s.peek()=='['&&data==']'){
+              s.pop();
+          }
+          else if(s.peek()=='('&&data==')'){
+              s.pop();
+          }
+          else if(s.peek()=='{'&&data=='}'){
+              s.pop();
+          }
+          else{
+              s.push(data);
+          }
+      }
+      
+      if(s.isEmpty()){
+          return true;
+      }
+      
+      return false;
+  }
 }
