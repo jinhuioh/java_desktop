d42cce28b034413ef470075d66878561563e89a2
OJH <aaa779979@naver.com>
2023-01-31 23:12:32 +0900

    codingTest

--- first_project/src/codingTest/codingTest4.java
+++ first_project/src/codingTest/codingTest4.java
@@ -8,18 +8,30 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-//방문길이 프로그래머스
-//class indexclass{
-//	int y;
-//	int x;
-//	int z;
-//	indexclass(int y, int x, int z){
-//		this.y = y;
-//		this.x = x;
-//		this.z = z;
-//	}
-//}
-
+//미로만들기
+//문제
+//n×n 바둑판 모양으로 총 n2개의 방이 있다. 일부분은 검은 방이고 나머지는 모두 흰 방이다. 검은 방은 사면이 벽으로 싸여 있어 들어갈 수 없다. 서로 붙어 있는 두 개의 흰 방 사이에는 문이 있어서 지나다닐 수 있다.
+//윗줄 맨 왼쪽 방은 시작방으로서 항상 흰 방이고, 아랫줄 맨 오른쪽 방은 끝방으로서 역시 흰 방이다.
+//
+//시작방에서 출발하여 길을 찾아서 끝방으로 가는 것이 목적인데, 아래 그림의 경우에는 시작방에서 끝 방으로 갈 수가 없다. 부득이 검은 방 몇 개를 흰 방으로 바꾸어야 하는데 되도록 적은 수의 방의 색을 바꾸고 싶다.
+//입력
+//첫 줄에는 한 줄에 들어가는 방의 수 n(1 ≤ n ≤ 50)이 주어지고, 다음 n개의 줄의 각 줄마다 0과 1이 이루어진 길이가 n인 수열이 주어진다. 0은 검은 방, 1은 흰 방을 나타낸다.
+//
+//출력
+//첫 줄에 흰 방으로 바꾸어야 할 최소의 검은 방의 수를 출력한다.
+//
+//예제 입력 1 
+//8
+//11100110
+//11010010
+//10011010
+//11101100
+//01000111
+//00110001
+//11011000
+//11000111
+//예제 출력 1 
+//2
 public class codingTest4 {
 //	static int[] dx = {0,0,1,-1};
 //	static int[] dy = {-1,1,0,0};
@@ -27,64 +39,5 @@ public class codingTest4 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String dirs = br.readLine();
-		boolean[][][] visited = new boolean[11][11][4];
-		Queue<int[]> q = new LinkedList<int[]>();
-//		int[][] map = new int[11][11];
-		
-		//dirs를 숫자로 바꾸어  q에 넣기
-		for(int i=0; i<dirs.length(); i++) {
-			char sone = dirs.charAt(i);
-			if(sone=='U') {
-				q.add(new int[] {1, 0, 0});
-			}
-			else if(sone=='L') {
-				q.add(new int[] {0,-1, 1});
-			}
-			else if (sone=='D') {
-				q.add(new int[] {-1, 0, 2});
-			}
-			else {
-				q.add(new int[] {0, 1, 3});
-			}
-		}
-		//map 위치 초기값.5,5부터 시작!
-		int b = 5;
-		int a = 5;
-		int answer = 0;
-		while (!q.isEmpty()) {
-			int y = q.peek()[0];
-			int x = q.peek()[1];
-			int z = q.peek()[2];
-			
-			q.remove();
-			
-			
-			int ny = y + b;
-			int nx = x + a;
-			
-			if(ny<0 || nx<0 || ny>=11 || nx>=11) continue; 
-			
-			if(!visited[ny][nx][z]) {
-				visited[ny][nx][z] = true;
-				if(z==0) {
-					visited[b][a][2] = true;
-				}
-				else if(z==1) {
-					visited[b][a][3] = true;
-				}
-				else if(z==2) {
-					visited[b][a][0] = true;
-				}
-				else {
-					visited[b][a][1] = true;
-				}
-				answer++;
-			}
-			b = ny;
-			a = nx;//위치 갱신
-			
-		}
-		System.out.println(answer);
-//		return answer;
 	}
 }
