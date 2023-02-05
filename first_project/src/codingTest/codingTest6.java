0ca12e60e20d7c4cf1be659ef91ce505e2a04088
OJH <aaa779979@naver.com>
2023-02-05 19:54:25 +0900

    codingTest

--- first_project/src/codingTest/codingTest6.java
+++ first_project/src/codingTest/codingTest6.java
@@ -7,71 +7,136 @@ import java.util.*;
 
 import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
 
-//			문제 설명
-//			압축
-//			신입사원 어피치는 카카오톡으로 전송되는 메시지를 압축하여 전송 효율을 높이는 업무를 맡게 되었다. 메시지를 압축하더라도 전달되는 정보가 바뀌어서는 안 되므로, 압축 전의 정보를 완벽하게 복원 가능한 무손실 압축 알고리즘을 구현하기로 했다.
+//문제 설명
+//연구소
+//문제
+//인체에 치명적인 바이러스를 연구하던 연구소에서 바이러스가 유출되었다. 다행히 바이러스는 아직 퍼지지 않았고, 바이러스의 확산을 막기 위해서 연구소에 벽을 세우려고 한다.
 //
-//			어피치는 여러 압축 알고리즘 중에서 성능이 좋고 구현이 간단한 LZW(Lempel–Ziv–Welch) 압축을 구현하기로 했다. LZW 압축은 1983년 발표된 알고리즘으로, 이미지 파일 포맷인 GIF 등 다양한 응용에서 사용되었다.
+//연구소는 크기가 N×M인 직사각형으로 나타낼 수 있으며, 직사각형은 1×1 크기의 정사각형으로 나누어져 있다. 연구소는 빈 칸, 벽으로 이루어져 있으며, 벽은 칸 하나를 가득 차지한다. 
 //
-//			LZW 압축은 다음 과정을 거친다.
+//일부 칸은 바이러스가 존재하며, 이 바이러스는 상하좌우로 인접한 빈 칸으로 모두 퍼져나갈 수 있다. 새로 세울 수 있는 벽의 개수는 3개이며, 꼭 3개를 세워야 한다.
+//0은 빈 칸, 1은 벽, 2는 바이러스가 있는 곳이다. 아무런 벽을 세우지 않는다면, 바이러스는 모든 빈 칸으로 퍼져나갈 수 있다.
+//연구소의 지도가 주어졌을 때 얻을 수 있는 안전 영역 크기의 최댓값을 구하는 프로그램을 작성하시오.
+//입력
+//첫째 줄에 지도의 세로 크기 N과 가로 크기 M이 주어진다. (3 ≤ N, M ≤ 8)
 //
-//			길이가 1인 모든 단어를 포함하도록 사전을 초기화한다.
-//			사전에서 현재 입력과 일치하는 가장 긴 문자열 w를 찾는다.
-//			w에 해당하는 사전의 색인 번호를 출력하고, 입력에서 w를 제거한다.
-//			입력에서 처리되지 않은 다음 글자가 남아있다면(c), w+c에 해당하는 단어를 사전에 등록한다.
-//			단계 2로 돌아간다.
-//			압축 알고리즘이 영문 대문자만 처리한다고 할 때, 사전은 다음과 같이 초기화된다. 사전의 색인 번호는 정수값으로 주어지며, 1부터 시작한다고 하자.
+//둘째 줄부터 N개의 줄에 지도의 모양이 주어진다. 0은 빈 칸, 1은 벽, 2는 바이러스가 있는 위치이다. 2의 개수는 2보다 크거나 같고, 10보다 작거나 같은 자연수이다.
 //
-//			색인 번호	1	2	3	...	24	25	26
-//			단어	A	B	C	...	X	Y	Z
-//			예를 들어 입력으로 KAKAO가 들어온다고 하자.
+//빈 칸의 개수는 3개 이상이다.
 //
-//			현재 사전에는 KAKAO의 첫 글자 K는 등록되어 있으나, 두 번째 글자까지인 KA는 없으므로, 첫 글자 K에 해당하는 색인 번호 11을 출력하고, 다음 글자인 A를 포함한 KA를 사전에 27 번째로 등록한다.
-//			두 번째 글자 A는 사전에 있으나, 세 번째 글자까지인 AK는 사전에 없으므로, A의 색인 번호 1을 출력하고, AK를 사전에 28 번째로 등록한다.
-//			세 번째 글자에서 시작하는 KA가 사전에 있으므로, KA에 해당하는 색인 번호 27을 출력하고, 다음 글자 O를 포함한 KAO를 29 번째로 등록한다.
-//			마지막으로 처리되지 않은 글자 O에 해당하는 색인 번호 15를 출력한다.
-//			현재 입력(w)	다음 글자(c)	출력	사전 추가(w+c)
-//			K	A	11	27: KA
-//			A	K	1	28: AK
-//			KA	O	27	29: KAO
-//			O		15	
-//			이 과정을 거쳐 다섯 글자의 문장 KAKAO가 4개의 색인 번호 [11, 1, 27, 15]로 압축된다.
-//
-//			입력으로 TOBEORNOTTOBEORTOBEORNOT가 들어오면 다음과 같이 압축이 진행된다.
-//
-//			현재 입력(w)	다음 글자(c)	출력	사전 추가(w+c)
-//			T	O	20	27: TO
-//			O	B	15	28: OB
-//			B	E	2	29: BE
-//			E	O	5	30: EO
-//			O	R	15	31: OR
-//			R	N	18	32: RN
-//			N	O	14	33: NO
-//			O	T	15	34: OT
-//			T	T	20	35: TT
-//			TO	B	27	36: TOB
-//			BE	O	29	37: BEO
-//			OR	T	31	38: ORT
-//			TOB	E	36	39: TOBE
-//			EO	R	30	40: EOR
-//			RN	O	32	41: RNO
-//			OT		34	
-//			입력 형식
-//			입력으로 영문 대문자로만 이뤄진 문자열 msg가 주어진다. msg의 길이는 1 글자 이상, 1000 글자 이하이다.
-//
-//			출력 형식
-//			주어진 문자열을 압축한 후의 사전 색인 번호를 배열로 출력하라.
-//
-//			입출력 예제
-//			msg	answer
-//			KAKAO	[11, 1, 27, 15]
-//			TOBEORNOTTOBEORTOBEORNOT	[20, 15, 2, 5, 15, 18, 14, 15, 20, 27, 29, 31, 36, 30, 32, 34]
-//			ABABABABABABABAB	[1, 2, 27, 29, 28, 31, 30]
+//출력
+//첫째 줄에 얻을 수 있는 안전 영역의 최대 크기를 출력한다.
+
+//2를 중심으로 상하좌우로 0 의 개수를 세서 0의 개수가 많은 2를 중심으로 벽을 세운다.
+
 public class codingTest6 {
+	    static final int dx[] = {0,0,1,-1};  //상하좌우 방향 설정
+	    static final int dy[] = {1,-1,0,0};  //상화좌우 방향 설정
+	    static int originalMap[][];
+	    static int n,m;
+	    static int maxSafeZone = Integer.MIN_VALUE; //최대값을 찾기 위한 최소값 설정
+
 		public static void main(String[] args) throws IOException {
-			
-		
+			 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		        StringTokenizer st = new StringTokenizer(br.readLine());
+
+		        n = Integer.parseInt(st.nextToken());
+		        m = Integer.parseInt(st.nextToken());
+		        originalMap = new int[n][m];
+
+		        for(int i=0; i<n; i++) {
+		            st = new StringTokenizer(br.readLine());
+		            for(int j=0; j<m; j++) {
+		                originalMap[i][j] = Integer.parseInt(st.nextToken());
+		            }
+		        }
+
+		        dfs(0);
+
+		        System.out.println(maxSafeZone);
+		    }
+
+		    static void dfs(int wallCnt) {
+		        //벽이 3개가 설치 된 경우 bfs 탐색 시작
+		        if(wallCnt == 3) {
+		            bfs();
+		            return;
+		        }
+
+		        for(int i=0; i<n; i++) {
+		            for(int j=0; j<m; j++) {
+		                if(originalMap[i][j] == 0) {
+		                    originalMap[i][j] = 1;
+		                    dfs(wallCnt+1);
+		                    originalMap[i][j] = 0;
+		                }
+		            }
+		        }
+		    }
+
+		    static void bfs() {
+		        Queue<Node> q = new LinkedList<>();
+
+		        for(int i=0; i<n; i++) {
+		            for(int j=0; j<m; j++) {
+		                if(originalMap[i][j] == 2) {
+		                    q.add(new Node(i,j));
+		                }
+		            }
+		        }
+
+		        //원본 연구소를 바꾸지 않기 위한 카피맵 사용
+		        int copyMap[][] = new int[n][m];
+
+		        for (int i = 0; i < n; i++) {
+		            copyMap[i] = originalMap[i].clone();
+		        }
+
+		        //BFS 탐색 시작
+		        while(!q.isEmpty()) {
+		            Node now = q.poll();
+		            int x = now.x; // 현재 값
+		            int y = now.y; //
+
+		            for(int k=0; k<4; k++) {
+		                int nx = x + dx[k];
+		                int ny = y + dy[k];
+
+		                //연구소 범위 밖이 아니고 빈칸일 경우에만 바이러스를 퍼트린다.
+		                if(0<=nx && nx<n && 0<=ny && ny<m) {
+		                    if(copyMap[nx][ny] == 0) {
+		                        q.add(new Node(nx,ny));
+		                        copyMap[nx][ny] = 2;
+		                    }
+		                }
+		            }
+		        }
+
+		        //SafeZone 확인
+		        funcSafeZone(copyMap);
+		    }
+
+		    private static void funcSafeZone(int[][] copyMap) {
+		        int safeZone =0;
+		        for(int i=0; i<n ; i++) {
+		            for(int j=0; j<m; j++) {
+		                if(copyMap[i][j] == 0) {
+		                    safeZone++;
+		                }
+		            }
+		        }
+		        if (maxSafeZone < safeZone) {
+		            maxSafeZone = safeZone;
+		        }
+		    }
+
+		    //Queue에 좌표값 x,y를 넣기 위함.
+		    static class Node {
+		        int x;
+		        int y;
+		        Node(int x, int y){
+		            this.x = x;
+		            this.y = y;
+		        }
+		    }
 		}
-
-}
-
