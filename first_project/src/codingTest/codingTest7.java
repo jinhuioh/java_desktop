199f97a7f85d362f9ca23ce2b566e806e83ff2ef
OJH <aaa779979@naver.com>
2023-02-13 20:11:41 +0900

    codingTest

--- first_project/src/codingTest/codingTest7.java
+++ first_project/src/codingTest/codingTest7.java
@@ -1,69 +1,93 @@
 package codingTest;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.Stack;
 import java.util.StringTokenizer;
-
-
+//맥주마시면서 걸어가기
+//문제
+//송도에 사는 상근이와 친구들은 송도에서 열리는 펜타포트 락 페스티벌에 가려고 한다. 올해는 맥주를 마시면서 걸어가기로 했다. 출발은 상근이네 집에서 하고, 맥주 한 박스를 들고 출발한다. 맥주 한 박스에는 맥주가 20개 들어있다. 목이 마르면 안되기 때문에 50미터에 한 병씩 마시려고 한다. 즉, 50미터를 가려면 그 직전에 맥주 한 병을 마셔야 한다.
+//
+//상근이의 집에서 페스티벌이 열리는 곳은 매우 먼 거리이다. 따라서, 맥주를 더 구매해야 할 수도 있다. 미리 인터넷으로 조사를 해보니 다행히도 맥주를 파는 편의점이 있다. 편의점에 들렸을 때, 빈 병은 버리고 새 맥주 병을 살 수 있다. 하지만, 박스에 들어있는 맥주는 20병을 넘을 수 없다. 편의점을 나선 직후에도 50미터를 가기 전에 맥주 한 병을 마셔야 한다.
+//
+//편의점, 상근이네 집, 펜타포트 락 페스티벌의 좌표가 주어진다. 상근이와 친구들이 행복하게 페스티벌에 도착할 수 있는지 구하는 프로그램을 작성하시오.
+//
+//입력
+//첫째 줄에 테스트 케이스의 개수 t가 주어진다. (t ≤ 50)
+//
+//각 테스트 케이스의 첫째 줄에는 맥주를 파는 편의점의 개수 n이 주어진다. (0 ≤ n ≤ 100).
+//
+//다음 n+2개 줄에는 상근이네 집, 편의점, 펜타포트 락 페스티벌 좌표가 주어진다. 각 좌표는 두 정수 x와 y로 이루어져 있다. (두 값 모두 미터, -32768 ≤ x, y ≤ 32767)
+//
+//송도는 직사각형 모양으로 생긴 도시이다. 두 좌표 사이의 거리는 x 좌표의 차이 + y 좌표의 차이 이다. (맨해튼 거리)
+//
+//출력
+//각 테스트 케이스에 대해서 상근이와 친구들이 행복하게 페스티벌에 갈 수 있으면 "happy", 중간에 맥주가 바닥나서 더 이동할 수 없으면 "sad"를 출력한다. 
 
 public class codingTest7 {
 	
-	public static void main(String[] args) throws IOException {
+	static int n,sx,sy,dx,dy;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = null;
+		
+		int t = Integer.parseInt(br.readLine());
+		for(int tc=0; tc<t; tc++) {
+			n = Integer.parseInt(br.readLine());
+			List<int[]> list = new ArrayList<>();
+			for(int i=0; i<n+2; i++) {
+				st = new StringTokenizer(br.readLine());
+				int x = Integer.parseInt(st.nextToken());
+				int y = Integer.parseInt(st.nextToken());
+				if(i==0) {
+					sx = x;
+					sy = y;
+				}else if(i==n+1) {
+					dx = x;
+					dy = y;
+				}else {
+					list.add(new int[]{x,y});
+				}
+			}
 			
+			bw.write(bfs(list)? "happy\n" : "sad\n");
+		}
+		
+		bw.flush();
+		bw.close();
 	}
-
-}
-
-//괄호문제
-class Solution1 {
-  public int solution(String s) {
-      int answer = 0;
-      String str=s;
-      
-      for(int i=0;i<s.length();i++){
-          char temp=str.charAt(0);
-          str=str.substring(1)+temp;
-          if(isCorrect(str)){
-              answer++;
-          }
-      }
-      
-      return answer;
-  }
-  
-  public boolean isCorrect (String str){
-      Stack<Character> s = new Stack<>();
-      
-      char[] arr = str.toCharArray();
-      
-      for(char data:arr){
-          if(s.isEmpty()){
-              s.push(data);
-          }
-          else if(s.peek()=='['&&data==']'){
-              s.pop();
-          }
-          else if(s.peek()=='('&&data==')'){
-              s.pop();
-          }
-          else if(s.peek()=='{'&&data=='}'){
-              s.pop();
-          }
-          else{
-              s.push(data);
-          }
-      }
-      
-      if(s.isEmpty()){
-          return true;
-      }
-      
-      return false;
-  }
+	
+	static boolean bfs(List<int[]> list) {
+		Queue<int[]> q = new LinkedList<>();
+		boolean[] visited = new boolean[n];
+		q.add(new int[] {sx,sy});
+		while(!q.isEmpty()) {
+			int[] pos = q.poll();
+			int px = pos[0], py = pos[1];
+			if(Math.abs(px-dx) + Math.abs(py-dy) <= 1000) {
+				return true;
+			}
+			
+			for(int i=0; i<n; i++) {
+				if(!visited[i]) {
+					int nx = list.get(i)[0], ny = list.get(i)[1];
+					int dis = Math.abs(px - nx) + Math.abs(py - ny);
+					if(dis <= 1000) {
+						visited[i] = true;
+						q.add(new int[]{nx,ny});
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
 }
