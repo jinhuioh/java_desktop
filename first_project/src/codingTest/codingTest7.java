76625087d65838eb3729eeb0ab49c7d0d4e27e5a
OJH <aaa779979@naver.com>
2023-02-22 20:35:23 +0900

    codingTest

--- first_project/src/codingTest/codingTest7.java
+++ first_project/src/codingTest/codingTest7.java
@@ -11,83 +11,100 @@ import java.util.List;
 import java.util.Queue;
 import java.util.Stack;
 import java.util.StringTokenizer;
-//맥주마시면서 걸어가기
 //문제
-//송도에 사는 상근이와 친구들은 송도에서 열리는 펜타포트 락 페스티벌에 가려고 한다. 올해는 맥주를 마시면서 걸어가기로 했다. 출발은 상근이네 집에서 하고, 맥주 한 박스를 들고 출발한다. 맥주 한 박스에는 맥주가 20개 들어있다. 목이 마르면 안되기 때문에 50미터에 한 병씩 마시려고 한다. 즉, 50미터를 가려면 그 직전에 맥주 한 병을 마셔야 한다.
-//
-//상근이의 집에서 페스티벌이 열리는 곳은 매우 먼 거리이다. 따라서, 맥주를 더 구매해야 할 수도 있다. 미리 인터넷으로 조사를 해보니 다행히도 맥주를 파는 편의점이 있다. 편의점에 들렸을 때, 빈 병은 버리고 새 맥주 병을 살 수 있다. 하지만, 박스에 들어있는 맥주는 20병을 넘을 수 없다. 편의점을 나선 직후에도 50미터를 가기 전에 맥주 한 병을 마셔야 한다.
-//
-//편의점, 상근이네 집, 펜타포트 락 페스티벌의 좌표가 주어진다. 상근이와 친구들이 행복하게 페스티벌에 도착할 수 있는지 구하는 프로그램을 작성하시오.
+//동규와 주미는 일직선 상의 돌 다리 위에있다. 돌의 번호는 0 부터 100,000 까지 존재하고 동규는 
+//\(N\)번 돌 위에, 주미는 
+//\(M\)번 돌 위에 위치하고 있다. 동규는 주미가 너무 보고싶기 때문에 최대한 빨리 주미에게 가기 위해 
+//\(A, B\) 만큼의 힘을 가진 스카이 콩콩을 가져왔다. 동규가 정한 다리를 건너는 규칙은 턴 방식인데, 한 턴에 이동할 수 있는 거리는 이러하다. 현 위치에서 +1칸, -1칸을 이동할 수 있고, 스카이 콩콩을 이용해 현 위치에서 
+//\(A\)나 
+//\(B\)만큼 좌우로 점프할 수 있으며, 순간적으로 힘을 모아 현 위치의 
+//\(A\)배나 
+//\(B\)배의 위치로 이동을 할 수 있다. 예를 들어 지금 동규가 7번 돌 위에 있고 스카이 콩콩의 힘이 8이면 그냥 점프를 해서 15번 돌에 갈 수도 있고, 순간적으로 힘을 모아 56번 돌에 갈 수도 있다는 것이다. 주어진 8가지의 방법 중 적절한 방법을 골라서 최대한 빨리 동규가 주미를 만날 수 있게 도와주자. 단, 이동 과정에서 100,000보다 크거나 0보다 작은 번호의 돌은 존재하지 않으므로 갈 수 없고, 같은 방법을 계속 사용해도 되며 항상 도달할 수 있는 케이스만 주어진다.
 //
 //입력
-//첫째 줄에 테스트 케이스의 개수 t가 주어진다. (t ≤ 50)
-//
-//각 테스트 케이스의 첫째 줄에는 맥주를 파는 편의점의 개수 n이 주어진다. (0 ≤ n ≤ 100).
-//
-//다음 n+2개 줄에는 상근이네 집, 편의점, 펜타포트 락 페스티벌 좌표가 주어진다. 각 좌표는 두 정수 x와 y로 이루어져 있다. (두 값 모두 미터, -32768 ≤ x, y ≤ 32767)
-//
-//송도는 직사각형 모양으로 생긴 도시이다. 두 좌표 사이의 거리는 x 좌표의 차이 + y 좌표의 차이 이다. (맨해튼 거리)
+//입력의 첫 줄에 스카이 콩콩의 힘 
+//\(A\)와 
+//\(B\), 그리고 동규의 현재위치 
+//\(N\), 주미의 현재 위치 
+//\(M\)이 주어진다. (단, 
+//\(2 \le A, B \le 30\) 이고  
+//\(0 \le N, M \le 100,000\))
 //
 //출력
-//각 테스트 케이스에 대해서 상근이와 친구들이 행복하게 페스티벌에 갈 수 있으면 "happy", 중간에 맥주가 바닥나서 더 이동할 수 없으면 "sad"를 출력한다. 
-
+//동규가 주미에게 도달하기 위한 최소한의 이동 횟수를 출력하라.
+//
+//예제 입력 1 
+//2 3 1 20
+//예제 출력 1 
+//4
+class Point{
+	int x;
+	int y;
+	Point(int x, int y){//돌다리번호와 이동횟수가 들어감.
+		this.x = x;
+		this.y = y;
+	}
+}
 public class codingTest7 {
-	
-	static int n,sx,sy,dx,dy;
+	 static int A,B,N,M, result=0;
+	 static boolean visit[] = new boolean[100001];
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = null;
-		
-		int t = Integer.parseInt(br.readLine());
-		for(int tc=0; tc<t; tc++) {
-			n = Integer.parseInt(br.readLine());
-			List<int[]> list = new ArrayList<>();
-			for(int i=0; i<n+2; i++) {
-				st = new StringTokenizer(br.readLine());
-				int x = Integer.parseInt(st.nextToken());
-				int y = Integer.parseInt(st.nextToken());
-				if(i==0) {
-					sx = x;
-					sy = y;
-				}else if(i==n+1) {
-					dx = x;
-					dy = y;
-				}else {
-					list.add(new int[]{x,y});
-				}
-			}
-			
-			bw.write(bfs(list)? "happy\n" : "sad\n");
-		}
-		
-		bw.flush();
-		bw.close();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+	        A = Integer.parseInt(st.nextToken());
+	        B = Integer.parseInt(st.nextToken());
+	        N = Integer.parseInt(st.nextToken());
+	        M = Integer.parseInt(st.nextToken());
+	        
+	        bfs();
+	        System.out.println(result);
+	    }
+	    private static void bfs() {
+	        Queue<Point> queue = new LinkedList<Point>();
+	        queue.add(new Point(N,0));
+	        visit[N] = true;
+	        while(!queue.isEmpty()) {
+	            
+	            Point po = queue.poll();
+	            
+	            if(po.x == M) {
+	                result = po.y;//이동 횟수 리턴
+	                return;
+	            }
+	            
+	            if(po.x + 1 < 100001 && !visit[po.x+1]) {
+	                visit[po.x+1] = true;
+	                queue.add(new Point(po.x+1, po.y+1));
+	            }
+	            if(po.x - 1 >= 0 && !visit[po.x-1]) {
+	                visit[po.x-1] = true;
+	                queue.add(new Point(po.x-1, po.y+1));
+	            }
+	            if(po.x + A < 100001 && !visit[po.x+A]) {
+	                visit[po.x+A] = true;
+	                queue.add(new Point(po.x+A, po.y+1));
+	            }
+	            if(po.x - A >= 0 && !visit[po.x-A]) {
+	                visit[po.x-A] = true;
+	                queue.add(new Point(po.x-A, po.y+1));
+	            }
+	            if(po.x + B < 100001 && !visit[po.x+B]) {
+	                visit[po.x+B] = true;
+	                queue.add(new Point(po.x+B, po.y+1));
+	            }
+	            if(po.x - B >= 0 && !visit[po.x-B]) {
+	                visit[po.x-B] = true;
+	                queue.add(new Point(po.x-B, po.y+1));
+	            }
+	            if(po.x * A < 100001 && !visit[po.x*A]) {
+	                visit[po.x*A] = true;
+	                queue.add(new Point(po.x*A, po.y+1));
+	            }
+	            if(po.x * B < 100001 && !visit[po.x*B]) {
+	                visit[po.x*B] = true;
+	                queue.add(new Point(po.x*B, po.y+1));
+	            }
+	        }
+	    }
 	}
-	
-	static boolean bfs(List<int[]> list) {
-		Queue<int[]> q = new LinkedList<>();
-		boolean[] visited = new boolean[n];
-		q.add(new int[] {sx,sy});
-		while(!q.isEmpty()) {
-			int[] pos = q.poll();
-			int px = pos[0], py = pos[1];
-			if(Math.abs(px-dx) + Math.abs(py-dy) <= 1000) {
-				return true;
-			}
-			
-			for(int i=0; i<n; i++) {
-				if(!visited[i]) {
-					int nx = list.get(i)[0], ny = list.get(i)[1];
-					int dis = Math.abs(px - nx) + Math.abs(py - ny);
-					if(dis <= 1000) {
-						visited[i] = true;
-						q.add(new int[]{nx,ny});
-					}
-				}
-			}
-		}
-		return false;
-	}
-	
-}
+	 
