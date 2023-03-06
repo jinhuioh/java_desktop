cb1e3abe558b5a4e26c5f228a282ad5b7fe04cf3
OJH <aaa779979@naver.com>
2023-03-06 21:54:37 +0900

    codingTest

--- first_project/src/codingTest/codingTest6.java
+++ first_project/src/codingTest/codingTest6.java
@@ -1,25 +1,44 @@
 package codingTest;
 import java.io.*;
 import java.util.*;
-//문제: 컴백홈 복습. 가짓수 구하는 문제 풀이방법
-//한수는 캠프를 마치고 집에 돌아가려 한다. 한수는 현재 왼쪽 아래점에 있고 집은 오른쪽 위에 있다. 그리고 한수는 집에 돌아가는 방법이 다양하다.
-//단, 한수는 똑똑하여 한번 지나친 곳을 다시 방문하지는 않는다.
-//위 예제는 한수가 집에 돌아갈 수 있는 모든 경우를 나타낸 것이다. T로 표시된 부분은 가지 못하는 부분이다.
-//문제는 R x C 맵에 못가는 부분이 주어지고 거리 K가 주어지면 한수가 집까지도 도착하는 경우 중 거리가 K인 가짓수를 구하는 것이다.
+//문제: 헌내기는 친구가 필요해
+//2020년에 입학한 헌내기 도연이가 있다. 도연이는 비대면 수업 때문에 학교에 가지 못해 학교에 아는 친구가 없었다. 드디어 대면 수업을 하게 된 도연이는 어서 캠퍼스 내의 사람들과 친해지고 싶다. 
+//
+//도연이가 다니는 대학의 캠퍼스는 
+//$N \times M$ 크기이며 캠퍼스에서 이동하는 방법은 벽이 아닌 상하좌우로 이동하는 것이다. 예를 들어, 도연이가 (
+//$x$, 
+//$y$)에 있다면 이동할 수 있는 곳은 (
+//$x+1$, 
+//$y$), (
+//$x$, 
+//$y+1$), (
+//$x-1$, 
+//$y$), (
+//$x$, 
+//$y-1$)이다. 단, 캠퍼스의 밖으로 이동할 수는 없다.
+//
+//불쌍한 도연이를 위하여 캠퍼스에서 도연이가 만날 수 있는 사람의 수를 출력하는 프로그램을 작성해보자.
 //
 //입력
-//첫 줄에 정수 R(1 ≤ R ≤ 5), C(1 ≤ C ≤ 5), K(1 ≤ K ≤ R×C)가 공백으로 구분되어 주어진다. 두 번째부터 R+1번째 줄까지는 R×C 맵의 정보를 나타내는 '.'과 'T'로 구성된 길이가 C인 문자열이 주어진다.
+//첫째 줄에는 캠퍼스의 크기를 나타내는 두 정수 
+//$N$ (
+//$ 1 \leq N \leq 600$), 
+//$M$ (
+//$ 1 \leq M \leq 600$)이 주어진다.
+//
+//둘째 줄부터 
+//$N$개의 줄에는 캠퍼스의 정보들이 주어진다. O는 빈 공간, X는 벽, I는 도연이, P는 사람이다. I가 한 번만 주어짐이 보장된다.
 //
 //출력
-//첫 줄에 거리가 K인 가짓수를 출력한다.
+//첫째 줄에 도연이가 만날 수 있는 사람의 수를 출력한다. 단, 아무도 만나지 못한 경우 TT를 출력한다.
 //
 //예제 입력 1 
-//3 4 6
-//....
-//.T..
-//....
+//3 5
+//OOOPO
+//OIOOX
+//OOOXP
 //예제 출력 1 
-//4
+//1
 class node1{
 	int y,x;
 	node1(int y, int x){
@@ -28,58 +47,55 @@ class node1{
 	}
 }
 public class codingTest6 {
-	static int n,m,k;
+	static int n,m;
 	static char[][] map;
 	static boolean[][] visited;
-	static Queue<node1> q;
+	static Queue<node1> q = new LinkedList<>();
 	static int[] dy = {0,0,1,-1};
 	static int[] dx = {-1,1,0,0};
-	static int answer;
 	
-	private static void dfs(int i, int j, int moved) {
-		
-		q = new LinkedList<>();
-		q.add(new node1(i,j));
-		
+	private static int bfs() {
+		int answer = 0;
 		while(!q.isEmpty()) {
 			node1 qp = q.poll();
-			for(int t = 0; t<4; t++) {
-				int ny = qp.y + dy[t];
-				int nx = qp.x + dx[t];
-				if(ny<0 || nx<0 || ny>=n || nx>=m || map[ny][nx]=='T' || visited[ny][nx]) continue;
-
-				if(ny==0 && nx==m-1) {
-					if(k == moved+1) {
-						answer++;
-						return;
-					}
+			for(int k = 0; k<4; k++) {
+				int ny = qp.y + dy[k];
+				int nx = qp.x + dx[k];
+				if(ny<0 || nx<0 || ny>=n || nx>=m || visited[ny][nx] || map[ny][nx] == 'X') continue;
+				if(map[ny][nx] == 'P') {
+					answer++;
 				}
+				q.add(new node1(ny,nx));
 				visited[ny][nx] = true;
-				dfs(ny, nx, moved+1);
-				visited[ny][nx] = false;
 			}
 		}
 		
+		return answer;
 	}
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken()); 
-		m = Integer.parseInt(st.nextToken()); 
-		k = Integer.parseInt(st.nextToken()); 
-    
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
 		map = new char[n][m];
 		visited = new boolean[n][m];
+		
 		for(int i = 0; i<n; i++) {
 			String s = br.readLine();
 			for(int j = 0; j<m; j++) {
-				map[i][j] = s.charAt(j); 
+				map[i][j] = s.charAt(j);
+				if(map[i][j]=='I') {
+					visited[i][j] = true;
+					q.add(new node1(i,j));
+				}
 			}
-		}//for
-		//함수호출
-		visited[n-1][0] = true;
-		dfs(n-1, 0, 1);
-		System.out.println(answer);
+		}
+		int answer = bfs();
+		if(answer == 0) {
+			System.out.println("TT");
+		}else {
+			System.out.println(answer);
+		}
 	}
 }
