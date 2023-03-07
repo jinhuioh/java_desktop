d48b73670e658ff49a02035fe6ca46fbea42626a
OJH <aaa779979@naver.com>
2023-03-07 21:43:22 +0900

    codingTest

--- first_project/src/codingTest/codingTest6.java
+++ first_project/src/codingTest/codingTest6.java
@@ -1,44 +1,33 @@
 package codingTest;
 import java.io.*;
 import java.util.*;
-//문제: 헌내기는 친구가 필요해
-//2020년에 입학한 헌내기 도연이가 있다. 도연이는 비대면 수업 때문에 학교에 가지 못해 학교에 아는 친구가 없었다. 드디어 대면 수업을 하게 된 도연이는 어서 캠퍼스 내의 사람들과 친해지고 싶다. 
+//문제 : 양치기 꿍
+//양치기 꿍은 맨날 늑대가 나타났다고 마을 사람들을 속였지만 이젠 더이상 마을 사람들이 속지 않는다. 화가 난 꿍은 복수심에 불타 아예 늑대들을 양들이 있는 울타리안에 마구 집어넣어 양들을 잡아먹게 했다.
 //
-//도연이가 다니는 대학의 캠퍼스는 
-//$N \times M$ 크기이며 캠퍼스에서 이동하는 방법은 벽이 아닌 상하좌우로 이동하는 것이다. 예를 들어, 도연이가 (
-//$x$, 
-//$y$)에 있다면 이동할 수 있는 곳은 (
-//$x+1$, 
-//$y$), (
-//$x$, 
-//$y+1$), (
-//$x-1$, 
-//$y$), (
-//$x$, 
-//$y-1$)이다. 단, 캠퍼스의 밖으로 이동할 수는 없다.
+//하지만 양들은 보통 양들이 아니다. 같은 울타리 영역 안의 양들의 숫자가 늑대의 숫자보다 더 많을 경우 늑대가 전부 잡아먹힌다. 물론 그 외의 경우는 양이 전부 잡아먹히겠지만 말이다.
 //
-//불쌍한 도연이를 위하여 캠퍼스에서 도연이가 만날 수 있는 사람의 수를 출력하는 프로그램을 작성해보자.
+//꿍은 워낙 똑똑했기 때문에 이들의 결과는 이미 알고있다. 만약 빈 공간을 '.'(점)으로 나타내고 울타리를 '#', 늑대를 'v', 양을 'k'라고 나타낸다면 여러분은 몇 마리의 양과 늑대가 살아남을지 계산할 수 있겠는가?
+//
+//단, 울타리로 막히지 않은 영역에는 양과 늑대가 없으며 양과 늑대는 대각선으로 이동할 수 없다.
 //
 //입력
-//첫째 줄에는 캠퍼스의 크기를 나타내는 두 정수 
-//$N$ (
-//$ 1 \leq N \leq 600$), 
-//$M$ (
-//$ 1 \leq M \leq 600$)이 주어진다.
+//입력의 첫 번째 줄에는 각각 영역의 세로와 가로의 길이를 나타내는 두 개의 정수 R, C (3 ≤ R, C ≤ 250)가 주어진다.
 //
-//둘째 줄부터 
-//$N$개의 줄에는 캠퍼스의 정보들이 주어진다. O는 빈 공간, X는 벽, I는 도연이, P는 사람이다. I가 한 번만 주어짐이 보장된다.
+//다음 각 R줄에는 C개의 문자가 주어지며 이들은 위에서 설명한 기호들이다.
 //
 //출력
-//첫째 줄에 도연이가 만날 수 있는 사람의 수를 출력한다. 단, 아무도 만나지 못한 경우 TT를 출력한다.
+//살아남게 되는 양과 늑대의 수를 각각 순서대로 출력한다.
 //
 //예제 입력 1 
-//3 5
-//OOOPO
-//OIOOX
-//OOOXP
+//6 6
+//...#..
+//.##v#.
+//#v.#.#
+//#.k#.#
+//.###.#
+//...###
 //예제 출력 1 
-//1
+//0 2
 class node1{
 	int y,x;
 	node1(int y, int x){
@@ -47,30 +36,50 @@ class node1{
 	}
 }
 public class codingTest6 {
-	static int n,m;
+	static int n,m, k_answer, v_answer;
 	static char[][] map;
 	static boolean[][] visited;
 	static Queue<node1> q = new LinkedList<>();
 	static int[] dy = {0,0,1,-1};
 	static int[] dx = {-1,1,0,0};
 	
-	private static int bfs() {
-		int answer = 0;
-		while(!q.isEmpty()) {
-			node1 qp = q.poll();
-			for(int k = 0; k<4; k++) {
-				int ny = qp.y + dy[k];
-				int nx = qp.x + dx[k];
-				if(ny<0 || nx<0 || ny>=n || nx>=m || visited[ny][nx] || map[ny][nx] == 'X') continue;
-				if(map[ny][nx] == 'P') {
-					answer++;
-				}
-				q.add(new node1(ny,nx));
-				visited[ny][nx] = true;
-			}
+	private static void bfs(int i, int j) {
+		int k_num = 0;
+		int v_num = 0;
+		if(map[i][j]=='v') {
+			v_num = 1;
+		}
+		else {
+			k_num = 1;
 		}
 		
-		return answer;
+		visited[i][j] = true;
+		q.add(new node1(i,j));
+		
+		while(!q.isEmpty()) {
+			node1 qp = q.poll();
+			for(int t = 0; t<4; t++) {
+				int ny = qp.y + dy[t];
+				int nx = qp.x + dx[t];
+				
+				if(ny<0|| nx<0 || ny>=n || nx>=m || map[ny][nx]=='#' || visited[ny][nx]) continue;
+				if(map[ny][nx] == 'v') {
+					v_num ++;
+				}
+				else if(map[ny][nx] == 'k') {
+					k_num++;
+				}
+				 visited[ny][nx] = true;
+				 q.add(new node1(ny,nx));
+				
+			}
+		}//while
+		if(v_num>=k_num) {
+			v_answer += v_num;
+		}
+		else {
+			k_answer += k_num;
+		}
 	}
 	
 	public static void main(String[] args) throws Exception {
@@ -84,18 +93,21 @@ public class codingTest6 {
 		for(int i = 0; i<n; i++) {
 			String s = br.readLine();
 			for(int j = 0; j<m; j++) {
-				map[i][j] = s.charAt(j);
-				if(map[i][j]=='I') {
-					visited[i][j] = true;
-					q.add(new node1(i,j));
+				map[i][j] = s.charAt(j); 
+			}//for
+		}//for
+		
+		k_answer = 0;
+		v_answer = 0;
+		for(int i = 0; i<n; i++) {
+			for(int j = 0; j<m; j++) {
+				if(map[i][j] == 'k' || map[i][j] == 'v') {
+					if(!visited[i][j]) {
+						bfs(i, j);
+					}
 				}
 			}
-		}
-		int answer = bfs();
-		if(answer == 0) {
-			System.out.println("TT");
-		}else {
-			System.out.println(answer);
-		}
+		}//for
+		System.out.println(k_answer +" "+ v_answer);
 	}
 }
